58b3d9b46ef1004518bf16f9d687c29c354368cb
gsemac <gabe.semac@gmail.com>
2020-06-09 15:15:10 -0600

    Added project Gsemac.Native

---
+++ Gsemac.Native/User32.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Gsemac.Native {
+
+    public static class User32 {
+
+        // Public members
+
+        public static IntPtr GetDC(IntPtr hwnd) {
+
+            return GetDCNative(hwnd);
+
+        }
+        public static IntPtr ReleaseDC(IntPtr hwnd, IntPtr hdc) {
+
+            return ReleaseDCNative(hwnd, hdc);
+
+        }
+
+        public static IntPtr SendMessage(IntPtr hWnd, int wMsg, IntPtr wParam, IntPtr lParam) {
+
+            return SendMessageNative(hWnd, wMsg, wParam, lParam);
+
+        }
+
+        // Private members
+
+        [DllImport("user32", EntryPoint = "GetDC")]
+        private static extern IntPtr GetDCNative(IntPtr hwnd);
+        [DllImport("user32", EntryPoint = "ReleaseDC")]
+        private static extern IntPtr ReleaseDCNative(IntPtr hwnd, IntPtr hdc);
+
+        [DllImport("user32", EntryPoint = "SendMessage")]
+        private static extern IntPtr SendMessageNative(IntPtr hWnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+    }
+
+}
