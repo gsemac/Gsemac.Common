291a4edb57671c68d3fa2680de574033f24235ab
gsemac <gabe.semac@gmail.com>
2020-06-11 22:43:18 -0600

    Added KeepAllLogRetentionPolicy class

---
+++ Gsemac.Logging/KeepAllLogRetentionPolicy.cs
@@ -0,0 +1,14 @@
+﻿namespace Gsemac.Logging {
+
+    public class KeepAllLogRetentionPolicy :
+        ILogRetentionPolicy {
+
+        public void ExecutePolicy(string directoryPath, string searchPattern = "*") {
+
+            // Do nothing; all log files are preserved.
+
+        }
+
+    }
+
+}
