301a6607e290fc96de6143aa9c1ad70344d2f55f
gsemac <gabe.semac@gmail.com>
2020-06-21 17:15:18 -0600

    Added GetVisibleRowBounds and improved GetVisibleRowsBounds

--- Gsemac.Forms/Utilities/DataGridViewUtilities.cs
+++ Gsemac.Forms/Utilities/DataGridViewUtilities.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Gsemac.Forms.Utilities {
@@ -31,15 +33,62 @@ namespace Gsemac.Forms.Utilities {
             return visibleRows;
 
         }
-        public static Rectangle GetVisibleRowsBounds(DataGridView dataGridView) {
+
+        public static Rectangle GetVisibleRowBounds(DataGridView dataGridView, int rowIndex, bool includeHeaders = true) {
+
+            Debug.Assert(rowIndex >= 0);
+            Debug.Assert(rowIndex < dataGridView.Rows.Count);
+
+            if (!dataGridView.Rows[rowIndex].Visible || dataGridView.Columns.Count <= 0)
+                return new Rectangle(0, 0, 0, 0);
+
+            Rectangle rowBounds = dataGridView.GetRowDisplayRectangle(rowIndex, true);
+
+            // GetRowDisplayRectangle returns the entire row rectangle, whether or not there are actually columns all the way across.
+            // Subtract the area beyond the last column.
+
+            Rectangle lastColumnBounds = dataGridView.GetColumnDisplayRectangle(dataGridView.Columns.GetLastColumn(DataGridViewElementStates.Visible, DataGridViewElementStates.None).Index, true);
+
+            int rowRight = rowBounds.X + rowBounds.Width;
+            int lastColumnRight = lastColumnBounds.X + lastColumnBounds.Width;
+
+            if (lastColumnRight < rowRight)
+                rowBounds = new Rectangle(rowBounds.X, rowBounds.Y, rowBounds.Width - (rowRight - lastColumnRight), rowBounds.Height);
+
+            // If headers are not included, subtract the row reader.
+
+            if (!includeHeaders && dataGridView.RowHeadersVisible)
+                rowBounds = new Rectangle(rowBounds.X + dataGridView.RowHeadersWidth, rowBounds.Y, rowBounds.Width - dataGridView.RowHeadersWidth, rowBounds.Height);
+
+            return rowBounds;
+
+        }
+        public static Rectangle GetVisibleRowsBounds(DataGridView dataGridView, bool includeHeaders = true) {
+
+            IEnumerable<DataGridViewRow> visibleRows = GetVisibleRows(dataGridView);
+
+            // If there are no visible rows, an empty rectangle will be returned.
 
             Rectangle bounds = new Rectangle(0, 0, 0, 0);
 
-            foreach (DataGridViewRow row in GetVisibleRows(dataGridView)) {
+            if (visibleRows.Count() > 0)
+                bounds = new Rectangle(int.MaxValue, int.MaxValue, 0, 0);
 
-                Rectangle rowBounds = dataGridView.GetRowDisplayRectangle(row.Index, true);
+            // If headers are included, include the column headers.
 
-                bounds = new Rectangle(0, 0, Math.Max(bounds.Width, rowBounds.Width), bounds.Height + rowBounds.Height);
+            if (includeHeaders)
+                bounds = new Rectangle(bounds.X, 0, bounds.Width, dataGridView.ColumnHeadersHeight);
+
+            foreach (DataGridViewRow row in visibleRows) {
+
+                Rectangle rowBounds = GetVisibleRowBounds(dataGridView, row.Index, includeHeaders);
+
+                bounds = new Rectangle(
+                    Math.Min(bounds.X, rowBounds.X),
+                    Math.Min(bounds.Y, rowBounds.Y),
+                    Math.Max(bounds.Width, rowBounds.Width),
+                    bounds.Height + rowBounds.Height
+                    );
 
             }
 
