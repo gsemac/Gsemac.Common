8e98e964184e622bcf659c79fda037124dcdbbf9
gsemac <gabe.semac@gmail.com>
2020-06-21 19:04:48 -0600

    Added ControlUtilities

---
+++ Gsemac.Forms/Utilities/ControlUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Windows.Forms;
+
+namespace Gsemac.Forms.Utilities {
+
+    public static class ControlUtilities {
+
+        public static void SetDoubleBuffered(Control control, bool value) {
+
+            control.GetType()
+                .GetProperty("DoubleBuffered", BindingFlags.Instance | BindingFlags.NonPublic)
+                .SetValue(control, value, null);
+
+        }
+
+    }
+
+}
