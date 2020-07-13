c3f5b92d327826aa00d11c2231d8fee958ebbb65
gsemac <gabe.semac@gmail.com>
2020-07-13 17:00:57 -0600

    Added ListUtilities

---
+++ Gsemac.Utilities/ListUtilities.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Gsemac.Utilities {
+
+    public static class ListUtilities {
+
+        public static void RemoveDuplicates<T>(List<T> items) {
+
+            HashSet<T> itemsLookup = new HashSet<T>();
+            List<T> distinctItems = new List<T>(items.Count);
+
+            foreach (T item in items) {
+
+                if (!itemsLookup.Contains(item)) {
+
+                    itemsLookup.Add(item);
+                    distinctItems.Add(item);
+
+                }
+
+            }
+
+            items.Clear();
+
+            items.AddRange(distinctItems);
+
+        }
+
+    }
+
+}
