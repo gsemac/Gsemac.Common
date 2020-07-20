c9b4f2573d2d1d82f8adfa3a72f692bd7c118c0b
gsemac <gabe.semac@gmail.com>
2020-07-20 14:50:36 -0600

    Added EnumerableExtensions

---
+++ Gsemac.Collections/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gsemac.Collections.Extensions {
+
+    public static class EnumerableExtensions {
+
+        public static IEnumerable<T> TakeLast<T>(this IEnumerable<T> source, int length) {
+
+            return source.Skip(Math.Max(0, source.Count() - length));
+
+        }
+
+    }
+
+}
