4ae7a567900ab2d9fb23c49b67b294b62916f952
gsemac <gabe.semac@gmail.com>
2020-07-25 02:45:42 -0600

    Added ScreenshotPage and ScreenshotElement to WebDriverUtilities

--- Gsemac.Net.SeleniumUtilities/WebDriverUtilities.cs
+++ Gsemac.Net.SeleniumUtilities/WebDriverUtilities.cs
@@ -2,6 +2,10 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
 using System.Net;
 
 namespace Gsemac.Net.SeleniumUtilities {
@@ -146,6 +150,33 @@ namespace Gsemac.Net.SeleniumUtilities {
 
         }
 
+        public static Size GetDocumentSize(IWebDriver driver) {
+
+            int totalWidth = (int)(long)((IJavaScriptExecutor)driver).ExecuteScript("return document.body.offsetWidth");
+            int totalHeight = (int)(long)((IJavaScriptExecutor)driver).ExecuteScript("return document.body.parentNode.scrollHeight");
+
+            return new Size(totalWidth, totalHeight);
+
+        }
+        public static Size GetViewportSize(IWebDriver driver) {
+
+            int viewportWidth = (int)(long)((IJavaScriptExecutor)driver).ExecuteScript("return document.body.clientWidth");
+            int viewportHeight = (int)(long)((IJavaScriptExecutor)driver).ExecuteScript("return window.innerHeight");
+
+            return new Size(viewportWidth, viewportHeight);
+
+        }
+        public static void ScrollTo(IWebDriver driver, int x, int y) {
+
+            ((IJavaScriptExecutor)driver).ExecuteScript($"window.scrollTo({x}, {y});");
+
+        }
+        public static void ScrollBy(IWebDriver driver, int deltaX, int deltaY) {
+
+            ((IJavaScriptExecutor)driver).ExecuteScript($"window.scrollBy({deltaX}, {deltaY});");
+
+        }
+
         public static void HideOtherElements(IWebDriver driver, string elementXPath) {
 
             IJavaScriptExecutor javascriptExecutor = (IJavaScriptExecutor)driver;
@@ -163,6 +194,152 @@ namespace Gsemac.Net.SeleniumUtilities {
 
 
         }
+
+#if NETFRAMEWORK
+
+        public static Bitmap ScreenshotPage(IWebDriver driver) {
+
+            // This implementation was adapted from https://stackoverflow.com/a/31396164/5383169 (Lachlan Goodhew-Cook)
+
+            // Maximize the driver window to maximize the amount of visible content.
+
+            Size originalSize = driver.Manage().Window.Size;
+
+            driver.Manage().Window.Maximize();
+
+            // By default, the screenshot will only contain the window's visible content.
+            // In order to get a full page screenshot, we need to take multiple screenshots and stitch them together.
+
+            Size documentSize = GetDocumentSize(driver);
+            Size viewportSize = GetViewportSize(driver);
+
+            ScrollTo(driver, 0, 0);
+
+            Bitmap result;
+
+            if (documentSize.Width <= viewportSize.Width && documentSize.Height < viewportSize.Height) {
+
+                // The entire document fits the screen, so there is no need to take multiple screenshots.
+
+                result = ScreenshotToBitmap((driver as ITakesScreenshot).GetScreenshot());
+
+            }
+            else {
+
+                // Divide the document area into rectangles, with each one representing the area of an individual screenshot.
+
+                List<Rectangle> rectangles = new List<Rectangle>();
+
+                for (int y = 0; y < documentSize.Height; y += viewportSize.Height) {
+
+                    int height = viewportSize.Height;
+
+                    if (y + viewportSize.Height > documentSize.Height)
+                        height = documentSize.Height - y;
+
+                    for (var x = 0; x < documentSize.Width; x += viewportSize.Width) {
+
+                        int width = viewportSize.Width;
+
+                        if (x + viewportSize.Width > documentSize.Width)
+                            width = documentSize.Width - x;
+
+                        rectangles.Add(new Rectangle(x, y, width, height));
+                    }
+
+                }
+
+                // Build the image.
+
+                result = new Bitmap(documentSize.Width, documentSize.Height);
+
+                Rectangle previousRectangle = Rectangle.Empty;
+
+                foreach (Rectangle rectangle in rectangles) {
+
+                    if (previousRectangle != Rectangle.Empty) {
+
+                        var deltaX = rectangle.Right - previousRectangle.Right;
+                        var deltaY = rectangle.Bottom - previousRectangle.Bottom;
+
+                        ScrollBy(driver, deltaX, deltaY);
+
+                    }
+
+                    Rectangle sourceRectangle = new Rectangle(viewportSize.Width - rectangle.Width, viewportSize.Height - rectangle.Height, rectangle.Width, rectangle.Height);
+
+                    using (Bitmap screenshot = ScreenshotToBitmap(TakeScreenshot(driver)))
+                    using (Graphics graphics = Graphics.FromImage(result))
+                        graphics.DrawImage(screenshot, rectangle, sourceRectangle, GraphicsUnit.Pixel);
+
+                    previousRectangle = rectangle;
+
+                }
+
+            }
+
+            // Restore the original window size.
+
+            driver.Manage().Window.Size = originalSize;
+
+            // Return the result.
+
+            return result;
+
+        }
+        public static Bitmap ScreenshotElement(IWebDriver driver, string elementXPath) {
+
+            // Maximize the driver window to maximize the amount of visible content.
+            // Even though this is done by ScreenshotPage, it's done here as well so that the element bounds are consistent.
+
+            Size originalSize = driver.Manage().Window.Size;
+
+            driver.Manage().Window.Maximize();
+
+            // Hide all elements except for the one we're interested in.
+
+            HideOtherElements(driver, elementXPath);
+
+            IWebElement element = driver.FindElements(By.XPath(elementXPath)).FirstOrDefault();
+            Rectangle elementRect = new Rectangle(element.Location.X, element.Location.Y, element.Size.Width, element.Size.Height);
+
+            Bitmap result;
+
+            using (Bitmap fullPageScreenshot = ScreenshotPage(driver))
+                result = fullPageScreenshot.Clone(elementRect, fullPageScreenshot.PixelFormat);
+
+            // Restore the original window state.
+
+            RestoreHiddenElements(driver);
+
+            driver.Manage().Window.Size = originalSize;
+
+            // Return the result.
+
+            return result;
+
+        }
+
+#endif
+
+        // Private members
+
+        private static Screenshot TakeScreenshot(IWebDriver driver) {
+
+            return (driver as ITakesScreenshot).GetScreenshot();
+
+        }
+
+#if NETFRAMEWORK
+
+        private static Bitmap ScreenshotToBitmap(Screenshot screenshot) {
+
+            using (Stream stream = new MemoryStream(screenshot.AsByteArray))
+                return new Bitmap(stream);
+
+        }
+
+#endif
 
     }
 
