a62dc71b135b2c7ee710f21d877d9334cb64a5ab
gsemac <gabe.semac@gmail.com>
2020-07-26 17:18:39 -0600

    Driver now navigates to about:blank before being released back to pool

--- Gsemac.Net.SeleniumUtilities/FixedCapacityWebDriverPool.cs
+++ Gsemac.Net.SeleniumUtilities/FixedCapacityWebDriverPool.cs
@@ -33,6 +33,8 @@ namespace Gsemac.Net.SeleniumUtilities {
         }
         public void ReleaseWebDriver(IWebDriver webDriver) {
 
+            webDriver.Navigate().GoToUrl("about:blank");
+
             ReleaseWebDriverInternal(webDriver);
 
         }
