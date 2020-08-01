c565089732c62458eaf3ee116cd911fce5fd420c
gsemac <gabe.semac@gmail.com>
2020-07-31 19:26:42 -0600

    Added IndexOf to EnumerableExtensions

--- Gsemac.Collections/Extensions/EnumerableExtensions.cs
+++ Gsemac.Collections/Extensions/EnumerableExtensions.cs
@@ -11,6 +11,24 @@ namespace Gsemac.Collections.Extensions {
             return source.Skip(Math.Max(0, source.Count() - length));
 
         }
+        public static int IndexOf<T>(this IEnumerable<T> source, T value) {
+
+            int index = 0;
+
+            foreach (T element in source) {
+
+                if (element.Equals(value))
+                    return index;
+
+                ++index;
+
+            }
+
+            // If we reach this point, the desired value was not found.
+
+            return -1;
+
+        }
 
     }
 
