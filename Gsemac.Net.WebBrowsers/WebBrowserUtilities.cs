c0be1fbfad3610bde488b6ad4e412b4f596f4b42
gsemac <gabe.semac@gmail.com>
2020-10-26 02:54:09 -0600

    Add project files.

---
+++ Gsemac.Net.WebBrowsers/WebBrowserUtilities.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Gsemac.Net.WebBrowsers {
+
+    public static class WebBrowserUtilities {
+
+        public static IEnumerable<string> GetInstalledWebBrowserExecutablePaths() {
+
+            // A better way of detecting installed web browsers might be looking up their associated keys in the registry.
+
+            IEnumerable<string> driveDirectoryPaths = System.IO.DriveInfo.GetDrives().Select(info => info.RootDirectory.FullName);
+
+            IEnumerable<string> programFilesDirectoryPaths = new string[]{
+                @"Program Files",
+                @"Program Files (x86)"
+            }.SelectMany(path => driveDirectoryPaths.Select(drivePath => System.IO.Path.Combine(drivePath, path)));
+
+            IEnumerable<string> webBrowserExecutablePaths = new string[]{
+                @"Google\Chrome\Application\chrome.exe",
+                @"Mozilla Firefox\firefox.exe"
+            }.SelectMany(path => programFilesDirectoryPaths.Select(programFilesDirectoryPath => System.IO.Path.Combine(programFilesDirectoryPath, path)));
+
+            return webBrowserExecutablePaths.Where(path => System.IO.File.Exists(path));
+
+        }
+
+    }
+
+}
