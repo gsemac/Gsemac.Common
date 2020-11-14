234ab0869db6e9df64e1f06396751db71dc80fe4
gsemac <gabe.semac@gmail.com>
2020-11-14 02:58:44 -0800

    Added ToArray method to StreamExtensions

---
+++ Gsemac.IO/Extensions/StreamExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace Gsemac.IO.Extensions {
+
+    public static class StreamExtensions {
+
+        public static byte[] ToArray(this Stream stream) {
+
+            using (var memoryStream = new MemoryStream()) {
+
+                stream.CopyTo(memoryStream);
+
+                return memoryStream.GetBuffer();
+
+            }
+
+        }
+
+    }
+
+}
