7cde8157a37a298a8e70ba78a463b97566ac216d
gsemac <gabe.semac@gmail.com>
2020-11-14 20:47:59 -0800

    Added CollectionExtensions class

---
+++ Gsemac.Collections/Extensions/CollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Gsemac.Collections.Extensions {
+
+    public static class CollectionExtensions {
+
+        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items) {
+
+            if (collection is IList<T> list) {
+
+                list.AddRange(items);
+
+            }
+            else {
+
+                foreach (T item in items)
+                    collection.Add(item);
+
+            }
+
+        }
+
+    }
+
+}
