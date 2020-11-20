10392349098386b9e07fb619f76f5a08aeed6c4d
gsemac <gabe.semac@gmail.com>
2020-11-20 02:41:02 -0800

    Added JsWindowTests

---
+++ Gsemac.Net.Tests/JavaScript/JsWindowTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gsemac.Net.JavaScript.Tests {
+
+    [TestClass]
+    public class JsWindowTests {
+
+        [TestMethod]
+        public void TestBtoaWithAsciiString() {
+
+            Assert.AreEqual("aGVsbG8gd29ybGQ=", JsWindow.Btoa("hello world"));
+
+        }
+        [TestMethod]
+        public void TestAtobWithAsciiString() {
+
+            Assert.AreEqual("hello world", JsWindow.Atob("aGVsbG8gd29ybGQ="));
+
+        }
+
+    }
+
+}
