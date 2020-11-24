0155bf87043af65d233b2719e73d39d9299a9800
gsemac <gabe.semac@gmail.com>
2020-11-24 10:09:50 -0800

    Added IniLexerToken class

---
+++ Gsemac.Text/Ini/IIniLexerToken.cs
@@ -0,0 +1,21 @@
+﻿namespace Gsemac.Text.Ini {
+
+    public enum IniLexerTokenType {
+        SectionStart, // [
+        SectionName,
+        SectionEnd, // ]
+        PropertyName,
+        PropertyValueSeparator, // =
+        PropertyValue
+    }
+
+    public interface IIniLexerToken {
+
+        // Public members
+
+        IniLexerTokenType Type { get; }
+        string Value { get; }
+
+    }
+
+}
---
+++ Gsemac.Text/Ini/IniLexerToken.cs
@@ -0,0 +1,26 @@
+﻿namespace Gsemac.Text.Ini {
+
+    public class IniLexerToken :
+        IIniLexerToken {
+
+        // Public members
+
+        public IniLexerTokenType Type { get; }
+        public string Value { get; }
+
+        public IniLexerToken(IniLexerTokenType type, string value) {
+
+            this.Type = type;
+            this.Value = value;
+
+        }
+
+        public override string ToString() {
+
+            return $"{Type}: {Value}";
+
+        }
+
+    }
+
+}
