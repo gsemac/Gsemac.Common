3d55dd1ef6b068714909123654aae9b8bd22c756
gsemac <gabe.semac@gmail.com>
2020-11-26 04:09:54 -0800

    Merged Gsemac.Net.WebDrivers into Gsemac.Net project

--- Gsemac.Net/Properties/Resources.Designer.cs
+++ Gsemac.Net/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Gsemac.Net.WebDrivers.Properties {
+namespace Gsemac.Net.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Gsemac.Net.WebDrivers.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Gsemac.Net.WebDrivers.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Gsemac.Net.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
