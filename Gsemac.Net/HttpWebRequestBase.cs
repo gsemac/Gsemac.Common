d852556ec11571e56185ff997a74f73fc232a539
gsemac <gabe.semac@gmail.com>
2020-11-25 18:42:28 -0800

    Added HttpWebRequestBase and HttpWebResponseBase classes

---
+++ Gsemac.Net/HttpWebRequestBase.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Gsemac.Net {
+
+    public abstract class HttpWebRequestBase :
+        WebRequest,
+        IHttpWebRequest {
+
+        // Public members
+
+        // Properties overidden from WebRequest
+
+        public override long ContentLength {
+            get {
+
+                if (long.TryParse(Headers[HttpRequestHeader.ContentLength], out long result))
+                    return result;
+
+                return 0;
+
+            }
+            set => Headers[HttpRequestHeader.ContentLength] = value.ToString();
+        }
+        public override string ContentType {
+            get => Headers[HttpRequestHeader.ContentType];
+            set => Headers[HttpRequestHeader.ContentType] = value;
+        }
+        public override ICredentials Credentials { get; set; }
+        public override WebHeaderCollection Headers { get; set; } = new WebHeaderCollection();
+        public override string Method { get; set; }
+        public override IWebProxy Proxy { get; set; }
+        public override Uri RequestUri { get; }
+        public override int Timeout { get; set; } = (int)TimeSpan.FromSeconds(100).TotalMilliseconds; // 100 seconds is the default for HttpWebRequest
+
+        // Properties inherited from IHttpWebRequest
+
+        public string Accept {
+            get => Headers[HttpRequestHeader.Accept];
+            set => Headers[HttpRequestHeader.Accept] = value;
+        }
+        public Uri Address => RequestUri;
+        public bool AllowAutoRedirect { get; set; } = true;
+        public bool AllowWriteStreamBuffering { get; set; } = true;
+        public DecompressionMethods AutomaticDecompression { get; set; } = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+        public string Connection {
+            get => Headers[HttpRequestHeader.Connection];
+            set => Headers[HttpRequestHeader.Connection] = value;
+        }
+        public CookieContainer CookieContainer { get; set; } = new CookieContainer();
+        public DateTime Date {
+            get => DateTime.Parse(Headers[HttpRequestHeader.Date]);
+            set => Headers[HttpRequestHeader.Date] = string.Format("{0:ddd,' 'dd' 'MMM' 'yyyy' 'HH':'mm':'ss' 'K}", value);
+        }
+        public string Expect {
+            get => Headers[HttpRequestHeader.Expect];
+            set => Headers[HttpRequestHeader.Expect] = value;
+        }
+        public bool HaveResponse { get; protected set; } = false;
+        public string Host {
+            get => Headers[HttpRequestHeader.Host];
+            set => Headers[HttpRequestHeader.Host] = value;
+        }
+        public DateTime IfModifiedSince {
+            get => DateTime.Parse(Headers[HttpRequestHeader.IfModifiedSince]);
+            set => Headers[HttpRequestHeader.IfModifiedSince] = string.Format("{0:ddd,' 'dd' 'MMM' 'yyyy' 'HH':'mm':'ss' 'K}", value);
+        }
+        public bool KeepAlive { get; set; } = true;
+        public int MaximumAutomaticRedirections { get; set; } = 50;
+        public Version ProtocolVersion { get; set; }
+        public int ReadWriteTimeout { get; set; } = 300000;
+        public string Referer {
+            get => Headers[HttpRequestHeader.Referer];
+            set => Headers[HttpRequestHeader.Referer] = value;
+        }
+        public string TransferEncoding {
+            get => Headers[HttpRequestHeader.TransferEncoding];
+            set => Headers[HttpRequestHeader.TransferEncoding] = value;
+        }
+        public string UserAgent {
+            get => Headers[HttpRequestHeader.UserAgent];
+            set => Headers[HttpRequestHeader.UserAgent] = value;
+        }
+
+        public X509CertificateCollection ClientCertificates { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public HttpContinueDelegate ContinueDelegate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public ServicePoint ServicePoint => throw new NotImplementedException();
+        public int MaximumResponseHeadersLength { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string MediaType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool SendChunked { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool UnsafeAuthenticatedConnectionSharing { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool Pipelined { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void AddRange(int from, int to) {
+            throw new NotImplementedException();
+        }
+        public void AddRange(long from, long to) {
+            throw new NotImplementedException();
+        }
+        public void AddRange(int range) {
+            throw new NotImplementedException();
+        }
+        public void AddRange(long range) {
+            throw new NotImplementedException();
+        }
+        public void AddRange(string rangeSpecifier, long from, long to) {
+            throw new NotImplementedException();
+        }
+        public void AddRange(string rangeSpecifier, int range) {
+            throw new NotImplementedException();
+        }
+        public void AddRange(string rangeSpecifier, long range) {
+            throw new NotImplementedException();
+        }
+        public void AddRange(string rangeSpecifier, int from, int to) {
+            throw new NotImplementedException();
+        }
+
+        // Protected members
+
+        protected HttpWebRequestBase(Uri requestUri) {
+
+            this.RequestUri = requestUri;
+
+        }
+
+    }
+
+}
---
+++ Gsemac.Net/HttpWebResponseBase.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace Gsemac.Net {
+
+    public abstract class HttpWebResponseBase :
+        WebResponse,
+        IHttpWebResponse {
+
+        // Public members
+
+        // Properties overidden from WebRequest
+
+        public override long ContentLength {
+            get {
+
+                if (long.TryParse(Headers[HttpResponseHeader.ContentLength], out long result))
+                    return result;
+
+                return 0;
+
+            }
+            set => Headers[HttpResponseHeader.ContentLength] = value.ToString();
+        }
+        public override WebHeaderCollection Headers => headers;
+        public override string ContentType {
+            get => Headers[HttpResponseHeader.ContentType];
+            set => Headers[HttpResponseHeader.ContentType] = value;
+        }
+        public override bool IsFromCache { get; } = false;
+        public override bool IsMutuallyAuthenticated { get; } = false;
+        public override Uri ResponseUri => responseUri;
+
+        // Properties inherited from IHttpWebResponse
+
+        public string CharacterSet { get; protected set; }
+        public virtual CookieCollection Cookies {
+            get => cookies.GetCookies(ResponseUri);
+            set => cookies.Add(value);
+        }
+        public string ContentEncoding {
+            get => Headers[HttpResponseHeader.ContentEncoding];
+            set => Headers[HttpResponseHeader.ContentEncoding] = value;
+        }
+        public Version ProtocolVersion { get; protected set; }
+        public DateTime LastModified {
+            get => DateTime.Parse(Headers[HttpResponseHeader.LastModified]);
+            set => Headers[HttpResponseHeader.LastModified] = string.Format("{0:ddd,' 'dd' 'MMM' 'yyyy' 'HH':'mm':'ss' 'K}", value);
+        }
+        public string Method { get; protected set; }
+        public string Server {
+            get => Headers[HttpResponseHeader.Server];
+            set => Headers[HttpResponseHeader.Server] = value;
+        }
+        public HttpStatusCode StatusCode { get; protected set; } = 0;
+        public string StatusDescription { get; protected set; }
+
+        public override Stream GetResponseStream() {
+
+            return responseStream;
+
+        }
+        public override void Close() {
+
+            // IMPORTANT NOTE: 
+            // This method gets called by Dispose(), but WebClient doesn't call Dispose(). Instead, it calls Close() on the response stream directly (bad design IMO).  
+            // As a result, it's important that this method doesn't contain any important logic, and ONLY closes the response stream.
+
+            if (!streamClosed)
+                responseStream.Close();
+
+            streamClosed = true;
+
+        }
+
+        public string GetResponseHeader(string headerName) {
+
+            return Headers[headerName];
+
+        }
+
+        // Protected members
+
+        protected HttpWebResponseBase(Uri responseUri, Stream responseStream) {
+
+            this.responseUri = responseUri;
+            this.responseStream = responseStream;
+
+        }
+
+        // Private members
+
+        private readonly Uri responseUri;
+        private readonly WebHeaderCollection headers = new WebHeaderCollection();
+        private readonly CookieContainer cookies = new CookieContainer();
+        private readonly Stream responseStream;
+        private bool streamClosed = false;
+
+    }
+
+}
