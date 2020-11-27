54f67bd26492c5c8435038cc710e687cab9721b5
gsemac <gabe.semac@gmail.com>
2020-11-26 18:44:33 -0800

    Added Kernel32 class

---
+++ Gsemac.Native/Kernel32.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Gsemac.Native {
+
+    public static class Kernel32 {
+
+        // Public members
+
+        public static bool SetDllDirectory(string lpPathName) {
+
+            return SetDllDirectoryNative(lpPathName);
+
+        }
+
+        // Private members
+
+        [DllImport("kernel32", EntryPoint = "SetDllDirectory", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool SetDllDirectoryNative(string lpPathName);
+
+    }
+
+}
