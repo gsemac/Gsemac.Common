2e5785ef7bd27a21d9112778f34e76c355562204
gsemac <gabe.semac@gmail.com>
2020-11-30 07:29:37 -0800

    Added IArchive and IArchiveEntry classes

---
+++ src/Gsemac.IO/Compression/IArchive.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Gsemac.IO.Compression {
+
+    public interface IArchive {
+
+        void AddEntry(Stream stream, string pathInArchive);
+        void RemoveEntry(IArchiveEntry entry);
+        void ExtractEntry(IArchiveEntry entry, Stream outputStream);
+        IEnumerable<IArchiveEntry> GetEntries();
+
+        void SaveTo(Stream stream, Stream outputStream);
+
+    }
+
+}
---
+++ src/Gsemac.IO/Compression/IArchiveEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace Gsemac.IO.Compression {
+
+    public interface IArchiveEntry {
+
+        string Path { get; }
+
+    }
+
+}
