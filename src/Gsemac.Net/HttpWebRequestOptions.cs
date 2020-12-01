47f835042f1ab39c64bf6ca9edde6255d093216d
gsemac <gabe.semac@gmail.com>
2020-12-01 10:35:00 -0800

    Added IHttpWebRequestFactory and HttpWebRequestOptions

---
+++ src/Gsemac.Net/HttpWebRequestOptions.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace Gsemac.Net {
+
+    public class HttpWebRequestOptions :
+        IHttpWebRequestOptions {
+
+        public string Accept { get; set; } = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+        public string AcceptLanguage { get; set; } = "en-US,en;q=0.5";
+        public CookieContainer Cookies { get; set; } = new CookieContainer();
+        public IWebProxy Proxy { get; set; }
+        public string UserAgent { get; set; } = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36";
+
+    }
+
+}
---
+++ src/Gsemac.Net/IHttpWebRequestOptions.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace Gsemac.Net {
+
+    public interface IHttpWebRequestOptions {
+
+        string Accept { get; }
+        string AcceptLanguage { get; }
+        CookieContainer Cookies { get; }
+        IWebProxy Proxy { get; }
+        string UserAgent { get; }
+
+    }
+
+}
