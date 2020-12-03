134745e38a2ff713767813e2ac1c469d60098fd2
gsemac <gabe.semac@gmail.com>
2020-12-03 03:22:45 -0800

    Added Gsemac.Threading project

---
+++ src/Gsemac.Threading/Tasks/TaskUtilities.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gsemac.Threading.Tasks {
+
+    public static class TaskUtilities {
+
+        public static Task Delay(double milliseconds) {
+
+            // .NET Framework 4.0 an earlier do not have Task.Delay defined
+
+            TaskCompletionSource<bool> taskCompletionSource = new TaskCompletionSource<bool>();
+
+            Timer timer = new Timer(_ => taskCompletionSource.TrySetResult(true), null, (int)milliseconds, Timeout.Infinite);
+
+            return taskCompletionSource.Task;
+
+        }
+
+    }
+
+}
