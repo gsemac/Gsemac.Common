98fcfaccd3bb965f1fec23991f8b1ca7d7683b35
gsemac <gabe.semac@gmail.com>
2020-12-07 05:28:09 -0800

    Added RegexUtilities

---
+++ src/Gsemac.Text/RegexUtilities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Gsemac.Text {
+
+    public static class RegexUtilities {
+
+        public static bool IsRegexValid(string pattern) {
+
+            if (string.IsNullOrEmpty(pattern))
+                return false;
+
+            try {
+
+                Regex regex = new Regex(pattern);
+
+                return true;
+
+            }
+            catch (ArgumentException) {
+
+                return false;
+
+            }
+
+        }
+
+    }
+
+}
