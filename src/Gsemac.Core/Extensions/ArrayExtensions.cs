48b161981d2de25b2cc69a8a5d4a123d66eafb67
gsemac <gabe.semac@gmail.com>
2020-12-07 05:23:32 -0800

    Added ArrayExtensions

---
+++ src/Gsemac.Core/Extensions/ArrayExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Gsemac.Core.Extensions {
+
+    public static class ArrayExtensions {
+
+        public static void Rotate<T>(this T[] array, int offset) {
+
+            if (array is null)
+                throw new ArgumentNullException(nameof(array));
+
+            if (offset == 0 || array.Length <= 0 || offset % array.Length == 0)
+                return;
+
+            offset %= array.Length;
+
+            if (offset < 0)
+                offset = array.Length + offset;
+
+            Array.Reverse(array, 0, array.Length);
+            Array.Reverse(array, 0, offset);
+            Array.Reverse(array, offset, array.Length - offset);
+
+        }
+
+    }
+
+}
---
+++ test/Gsemac.Core.Tests/Extensions/ArrayExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gsemac.Core.Extensions.Tests {
+
+    [TestClass]
+    public class ArrayExtensionsTests {
+
+        [TestMethod]
+        public void TestRotateRight() {
+
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            array.Rotate(2);
+
+            CollectionAssert.AreEqual(new[] { 4, 5, 1, 2, 3 }, array);
+
+        }
+        [TestMethod]
+        public void TestRotateLeft() {
+
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            array.Rotate(-2);
+
+            CollectionAssert.AreEqual(new[] { 3, 4, 5, 1, 2 }, array);
+
+        }
+
+    }
+
+}
