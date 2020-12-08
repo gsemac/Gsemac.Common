bedbe6b1853f821e3637849fc6647938cac88032
gsemac <gabe.semac@gmail.com>
2020-12-08 09:32:30 -0800

    Added IImage interface

---
+++ src/Gsemac.Drawing/Imaging/IImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+
+namespace Gsemac.Drawing {
+
+    public interface IImage :
+        IDisposable {
+
+#if NETFRAMEWORK
+        Image ToBitmap();
+#endif
+
+    }
+
+}
---
+++ src/Gsemac.Drawing/Imaging/SystemDrawingImage.cs
@@ -0,0 +1,56 @@
+﻿#if NETFRAMEWORK
+
+using System.Drawing;
+
+namespace Gsemac.Drawing {
+
+    public class SystemDrawingImage :
+        IImage {
+
+        // Public members
+
+        public SystemDrawingImage(Image image) {
+
+            this.image = image;
+
+        }
+
+        public Image ToBitmap() {
+
+            return image;
+
+        }
+
+        public void Dispose() {
+
+            Dispose(disposing: true);
+
+            System.GC.SuppressFinalize(this);
+
+        }
+
+        // Private members
+
+        private readonly Image image;
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing) {
+
+            if (!disposedValue) {
+
+                if (disposing) {
+
+                    image.Dispose();
+
+                }
+
+                disposedValue = true;
+
+            }
+        }
+
+    }
+
+}
+
+#endif
