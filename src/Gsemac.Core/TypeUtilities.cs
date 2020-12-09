5fd54813ebe710aafd5d988b52b81840e231886b
gsemac <gabe.semac@gmail.com>
2020-12-09 12:54:51 -0800

    Added TypeUtilities class

---
+++ src/Gsemac.Core/TypeUtilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gsemac.Core {
+
+    public static class TypeUtilities {
+
+        public static IEnumerable<Type> GetTypesImplementingInterface<T>() {
+
+            Type interfaceType = typeof(T);
+
+            IEnumerable<Type> types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(assembly => assembly.GetTypes())
+                .Where(type => interfaceType.IsAssignableFrom(type) && !type.IsAbstract);
+
+            return types;
+
+        }
+
+    }
+
+}
