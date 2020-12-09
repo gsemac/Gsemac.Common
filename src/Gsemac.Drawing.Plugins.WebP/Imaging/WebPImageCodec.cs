07953a6ae78ba105e4a56f910402be5cf1ab6323
gsemac <gabe.semac@gmail.com>
2020-12-09 12:56:07 -0800

    Added Gsemac.Drawing.Plugins.WebP project

---
+++ src/Gsemac.Drawing.Plugins.WebP/Imaging/WebPImageCodec.cs
@@ -0,0 +1,58 @@
+﻿#if NETFRAMEWORK
+
+using Gsemac.IO.Extensions;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace Gsemac.Drawing.Imaging {
+
+    public class WebPImageCodec :
+        IImageCodec {
+
+        // Public members
+
+        public IEnumerable<IImageFormat> SupportedImageFormats => GetSupportedImageFormats();
+        public int Priority => 0;
+
+        public Image Decode(Stream stream) {
+
+            using (WebPWrapper.WebP decoder = new WebPWrapper.WebP())
+                return decoder.Decode(stream.ToArray());
+
+        }
+        IImage IImageDecoder.Decode(Stream stream) {
+
+            return new GdiImage(Decode(stream), this);
+
+        }
+        public void Encode(Image image, Stream stream, IImageEncoderOptions encoderOptions) {
+
+            using (WebPWrapper.WebP encoder = new WebPWrapper.WebP())
+            using (MemoryStream webPStream = new MemoryStream(encoder.EncodeLossy(image as Bitmap, encoderOptions.Quality)))
+                webPStream.CopyTo(stream);
+
+        }
+        public void Encode(IImage image, Stream stream, IImageEncoderOptions encoderOptions) {
+
+            using (Bitmap bitmap = image.ToBitmap())
+                Encode(bitmap, stream, encoderOptions);
+
+        }
+
+        // Private members
+
+        private IEnumerable<IImageFormat> GetSupportedImageFormats() {
+
+            return new[]{
+                ".webp"
+            }.Select(ext => ImageFormat.FromFileExtension(ext));
+
+        }
+
+    }
+
+}
+
+#endif
