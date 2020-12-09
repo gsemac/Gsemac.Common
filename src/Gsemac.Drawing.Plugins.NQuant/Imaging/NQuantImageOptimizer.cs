e561075db7e666cf60662dae46f7b8f0ea9317f1
gsemac <gabe.semac@gmail.com>
2020-12-09 12:55:38 -0800

    Added Gsemac.Drawing.Plugins.NQuant project

---
+++ src/Gsemac.Drawing.Plugins.NQuant/Imaging/NQuantImageOptimizer.cs
@@ -0,0 +1,59 @@
+﻿#if NETFRAMEWORK
+
+using nQuant;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace Gsemac.Drawing.Imaging {
+
+    public class NQuantImageOptimizer :
+        IImageOptimizer {
+
+        // Public members
+
+        public IEnumerable<IImageFormat> SupportedImageFormats => GetSupportedImageFormats();
+        public int Priority => 1;
+
+        public bool Optimize(Stream stream, ImageOptimizationMode optimizationMode) {
+
+            if (stream.CanRead && stream.CanSeek) {
+
+                WuQuantizer quantizer = new WuQuantizer();
+
+                using (Bitmap bitmap = (Bitmap)Image.FromStream(stream)) {
+
+                    using (Image quantized = quantizer.QuantizeImage(bitmap)) {
+
+                        stream.Seek(0, SeekOrigin.Begin);
+
+                        quantized.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+
+                    }
+
+                }
+
+                return true;
+
+            }
+
+            return false;
+
+        }
+
+        // Private members
+
+        private IEnumerable<IImageFormat> GetSupportedImageFormats() {
+
+            return new[]{
+                ".png"
+            }.Select(ext => ImageFormat.FromFileExtension(ext));
+
+        }
+
+    }
+
+}
+
+#endif
