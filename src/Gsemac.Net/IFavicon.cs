8789caa3bffce112ffd355aee25988d56ada3765
gsemac <gabe.semac@gmail.com>
2020-12-12 14:04:05 -0800

    Added IFavicon and IFaviconCollection interfaces

---
+++ src/Gsemac.Net/IFavicon.cs
@@ -0,0 +1,15 @@
+﻿using Gsemac.Drawing.Imaging;
+using System;
+
+namespace Gsemac.Net {
+
+    public interface IFavicon :
+        IDisposable {
+
+        Uri Uri { get; }
+        string Name { get; }
+        IImage Icon { get; }
+
+    }
+
+}
---
+++ src/Gsemac.Net/IFaviconCollection.cs
@@ -0,0 +1,24 @@
+﻿using Gsemac.Drawing.Imaging;
+using System;
+using System.Collections.Generic;
+
+namespace Gsemac.Net {
+
+    public interface IFaviconCollection :
+        ICollection<IFavicon>,
+        IDisposable {
+
+        IImage DefaultIcon { get; }
+
+        IFavicon GetFavicon(Uri uri);
+        IFavicon GetFavicon(string name);
+
+        IFavicon AddFromFile(string filePath);
+        IFavicon AddFromHtmlDocument(string htmlDocument);
+        IFavicon AddFromHtmlDocument(Uri uri, string htmlDocument);
+        IFavicon AddFromUri(Uri uri);
+        IFavicon AddFromUri(Uri uri, string name);
+
+    }
+
+}
