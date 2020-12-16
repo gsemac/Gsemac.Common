507f917a2fde58e78b418eaaa8e3a51d49e551b4
gsemac <gabe.semac@gmail.com>
2020-12-16 09:34:05 -0800

    Added DateExtensions class

---
+++ src/Gsemac.Core/Extensions/DateExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Gsemac.Core.Extensions {
+
+    public static class DateExtensions {
+
+        public static long ToUnixTimeSeconds(this DateTime date) {
+
+            return DateUtilities.ToUnixTimeSeconds(date);
+
+        }
+        public static long ToUnixTimeSeconds(this DateTimeOffset date) {
+
+            return DateUtilities.ToUnixTimeSeconds(date);
+
+        }
+
+    }
+
+}
