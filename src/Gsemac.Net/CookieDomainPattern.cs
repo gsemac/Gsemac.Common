413e4b69be6228a5ed6066a014c6f82a1c7685f9
gsemac <gabe.semac@gmail.com>
2020-12-16 20:24:23 -0800

    Added CookieDomainPattern class

---
+++ src/Gsemac.Net/CookieDomainPattern.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Gsemac.Net {
+
+    public class CookieDomainPattern {
+
+        // Public members
+
+        public CookieDomainPattern(string domain) {
+
+            domain = domain.TrimStart('.');
+
+            if (!domain.Contains("."))
+                throw new ArgumentException("The given pattern is invalid.", nameof(domain));
+
+            this.domain = domain;
+
+        }
+
+        public bool IsMatch(string uri) {
+
+            uri = uri.TrimStart('.').TrimEnd('/');
+
+            // Follow the comparison rules as described here: https://stackoverflow.com/a/1063760
+
+            Regex urlPartsPattern = new Regex(@"^(?:[^:]+:\/\/)?([^\/]+)(.*$)");
+
+            Match urlPartsMatch = urlPartsPattern.Match(uri);
+            Match cookieDomainPartsMatch = urlPartsPattern.Match(domain);
+
+            string hostname = urlPartsMatch.Groups[1].Value;
+            string path = urlPartsMatch.Groups[2].Value;
+            string cookieDomainHostname = cookieDomainPartsMatch.Groups[1].Value;
+            string cookieDomainPath = cookieDomainPartsMatch.Groups[2].Value;
+
+            // Note the domain should be case-insensitive, but the path should be case-sensitive: https://stackoverflow.com/a/400013
+
+            return (hostname.Equals(cookieDomainHostname, StringComparison.OrdinalIgnoreCase) || hostname.EndsWith("." + cookieDomainHostname, StringComparison.OrdinalIgnoreCase)) &&
+                path.Equals(cookieDomainPath);
+
+        }
+        public bool IsMatch(Uri uri) {
+
+            return IsMatch(uri.AbsoluteUri);
+
+        }
+
+        // Private members
+
+        private readonly string domain;
+
+    }
+
+}
---
+++ test/Gsemac.Net.Tests/CookieDomainEqualityComparerTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Gsemac.Net.Tests {
+
+    [TestClass]
+    public class CookieDomainEqualityComparerTests {
+
+        [TestMethod]
+        public void TestDomainAndDomainWithSubdomain() {
+
+            Assert.IsTrue(new CookieDomainPattern("example.com").IsMatch("www.example.com"));
+
+        }
+        [TestMethod]
+        public void TestDomainStartingWithDotAndDomainWithSubdomain() {
+
+            Assert.IsTrue(new CookieDomainPattern(".example.com").IsMatch("www.example.com"));
+
+        }
+        [TestMethod]
+        public void TestDomainStartingWithDotAndDomainWithoutSubdomain() {
+
+            Assert.IsTrue(new CookieDomainPattern(".example.com").IsMatch("example.com"));
+
+        }
+        [TestMethod]
+        public void TestDomainWithDifferentDomain() {
+
+            Assert.IsFalse(new CookieDomainPattern(".example.com").IsMatch("anotherexample.com"));
+
+        }
+        [TestMethod]
+        public void TestDomainWithDifferentSubdomain() {
+
+            Assert.IsFalse(new CookieDomainPattern("www.example.com").IsMatch("www2.example.com"));
+
+        }
+        [TestMethod]
+        public void TestDomainWithTld() {
+
+            Assert.IsFalse(new CookieDomainPattern("example.com").IsMatch(".com"));
+
+        }
+        [TestMethod]
+        public void TestTldWithTld() {
+
+            Assert.ThrowsException<ArgumentException>(() => new CookieDomainPattern(".com").IsMatch(".com"));
+
+        }
+
+    }
+
+}
