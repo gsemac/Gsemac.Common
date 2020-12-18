ab029c23653884ddf57ee0226c3ec2d787d9ca05
gsemac <gabe.semac@gmail.com>
2020-12-18 08:01:45 -0800

    Added WindowsExplorerSortComparer

---
+++ src/Gsemac.Win32/Collections/WindowsExplorerSortComparer.cs
@@ -0,0 +1,27 @@
+﻿using Gsemac.Win32;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Gsemac.Collections {
+
+    public class WindowsExplorerSortComparer :
+        IComparer<string>,
+        IComparer<FileInfo> {
+
+        public int Compare(string x, string y) {
+
+            x = x ?? "";
+            y = y ?? "";
+
+            return SHLWAPI.StrCmpLogicalW(x, y);
+
+        }
+        public int Compare(FileInfo x, FileInfo y) {
+
+            return Compare(x.FullName, y.FullName);
+
+        }
+
+    }
+
+}
---
+++ src/Gsemac.Win32/Win32/SHLWAPI.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Gsemac.Win32 {
+
+    public static class SHLWAPI {
+
+        // Public members
+
+        public static int StrCmpLogicalW(string psz1, string psz2) {
+
+            return StrCmpLogicalWNative(psz1, psz2);
+
+        }
+
+        // Private members
+
+        [DllImport("shlwapi.dll", EntryPoint = "StrCmpLogicalW", CharSet = CharSet.Unicode)]
+        public static extern int StrCmpLogicalWNative(string psz1, string psz2);
+
+    }
+
+}
