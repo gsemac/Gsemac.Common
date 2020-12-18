8b56e1522fe685dde6a096bb95524c4ab15bddcf
gsemac <gabe.semac@gmail.com>
2020-12-18 08:15:21 -0800

    Added IQueue interface

---
+++ src/Gsemac.Collections/IQueue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Gsemac.Collections {
+
+    public interface IQueue<T> :
+       IEnumerable,
+       IEnumerable<T> {
+
+        void Clear();
+        bool Contains(T item);
+        void CopyTo(T[] array, int arrayIndex);
+        T Dequeue();
+        void Enqueue(T item);
+        T Peek();
+        T[] ToArray();
+
+    }
+
+}
