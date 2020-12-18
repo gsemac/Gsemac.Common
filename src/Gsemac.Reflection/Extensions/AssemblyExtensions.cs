6c7842fdcdeb6e0c42a4651701e30d01c3283ba7
gsemac <gabe.semac@gmail.com>
2020-12-18 08:37:17 -0800

    Fixed GetLoadableTypes not returning any types

--- src/Gsemac.Reflection/Extensions/AssemblyExtensions.cs
+++ src/Gsemac.Reflection/Extensions/AssemblyExtensions.cs
@@ -22,7 +22,7 @@ namespace Gsemac.Reflection.Extensions {
 
             }
 
-            return result.Where(type => type != null && type.TypeInitializer != null);
+            return result.Where(type => type != null);
 
         }
 
