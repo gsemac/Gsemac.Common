cf8f1dcfb669ef43afc4f3100e82ab55f5513c7c
gsemac <gabe.semac@gmail.com>
2020-12-18 15:52:39 -0800

    NaturalSortComparer now implements IComparer

--- src/Gsemac.Collections/NaturalSortComparer.cs
+++ src/Gsemac.Collections/NaturalSortComparer.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 
 namespace Gsemac.Collections {
 
     public class NaturalSortComparer :
+        IComparer,
         IComparer<string>,
         IComparer<FileInfo> {
 
@@ -45,6 +47,14 @@ namespace Gsemac.Collections {
         public int Compare(FileInfo x, FileInfo y) {
 
             return Compare(x.FullName, y.FullName);
+
+        }
+        public int Compare(object x, object y) {
+
+            if (x is FileInfo fileInfoX && y is FileInfo fileInfoY)
+                return Compare(fileInfoX, fileInfoY);
+
+            return Compare(x.ToString(), y.ToString());
 
         }
 
