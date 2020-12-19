d07ad5ef1eb20002feefd8e3c01e23a4684e8197
gsemac <gabe.semac@gmail.com>
2020-12-18 19:04:42 -0800

    Added EnvironmentUtilities class

---
+++ src/Gsemac.Core/EnvironmentUtilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Gsemac.Core {
+
+    public static class EnvironmentUtilities {
+
+        public static void AddEnvironmentPath(string path) {
+
+            AddEnvironmentPaths(new[] { path });
+
+        }
+        public static void AddEnvironmentPaths(IEnumerable<string> paths) {
+
+            // https://stackoverflow.com/a/2864714 (Chris Schmich)
+
+            var oldPathValue = new[] { Environment.GetEnvironmentVariable("PATH") ?? string.Empty };
+            string newPathValue = string.Join(Path.PathSeparator.ToString(), oldPathValue.Concat(paths));
+
+            Environment.SetEnvironmentVariable("PATH", newPathValue);
+
+        }
+
+    }
+
+}
