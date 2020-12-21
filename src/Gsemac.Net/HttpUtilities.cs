f3c9e3647b3e47c439c8b4781b2c1763ddbeb43e
gsemac <gabe.semac@gmail.com>
2020-12-21 10:13:35 -0800

    Added HttpUtilities class

---
+++ src/Gsemac.Net/HttpUtilities.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+
+namespace Gsemac.Net {
+
+    public static class HttpUtilities {
+
+        public static HttpStatusCode? GetStatusCode(IHttpWebRequest webRequest) {
+
+            if (webRequest is null)
+                throw new ArgumentNullException(nameof(webRequest));
+
+            HttpStatusCode? statusCode = null;
+
+            // Attempt to make a HEAD request first. If it fails, we'll fall back to making a GET request.
+
+            webRequest.AllowAutoRedirect = false;
+
+            foreach (string method in new[] { "HEAD", "GET" }) {
+
+                webRequest.Method = method;
+
+                try {
+
+                    using (WebResponse webResponse = webRequest.GetResponse())
+                        statusCode = (webResponse as IHttpWebResponse)?.StatusCode;
+                    
+                }
+                catch (WebException ex) {
+
+                    using (WebResponse webResponse = ex.Response)
+                        statusCode = (webResponse as IHttpWebResponse)?.StatusCode;
+
+                }
+
+                if (statusCode.HasValue && statusCode != HttpStatusCode.MethodNotAllowed)
+                    break;
+
+            }
+
+            return statusCode;
+
+        }
+
+    }
+
+}
