02d667540096b8921881a7d1fe56b19d03bb0757
gsemac <gabe.semac@gmail.com>
2021-01-05 16:20:42 -0700

    Added DownloadFileSync method to WebClientExtensions

--- src/Gsemac.Net/Extensions/WebClientExtensions.cs
+++ src/Gsemac.Net/Extensions/WebClientExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using Gsemac.IO;
 using System;
+using System.ComponentModel;
 using System.Net;
+using System.Threading;
 
 namespace Gsemac.Net.Extensions {
 
@@ -14,6 +16,35 @@ namespace Gsemac.Net.Extensions {
         public static void DownloadFileAsync(this WebClient client, Uri address) {
 
             client.DownloadFileAsync(address, PathUtilities.GetFileName(address.AbsoluteUri));
+
+        }
+
+        public static void DownloadFileSync(this WebClient client, Uri address, string fileName) {
+
+            // Events are only fired when downloading asynchronously with DownloadFileAsync.
+            // This method allows for synchronous downloads while still firing events. 
+            // Based on the solution given here: https://stackoverflow.com/a/25834736 (user195275)
+
+            object mutex = new object();
+
+            void handleDownloadComplete(object sender, AsyncCompletedEventArgs e) {
+
+                lock (e.UserState)
+                    Monitor.Pulse(e.UserState);
+
+            }
+
+            client.DownloadFileCompleted += handleDownloadComplete;
+
+            lock (mutex) {
+
+                client.DownloadFileAsync(address, fileName, mutex);
+
+                Monitor.Wait(mutex);
+
+            }
+
+            client.DownloadFileCompleted -= handleDownloadComplete;
 
         }
 
