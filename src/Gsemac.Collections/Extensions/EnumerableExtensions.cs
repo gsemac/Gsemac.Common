eb7d7f31365c3c5587e0563dbb6abdc2c07d4814
gsemac <gabe.semac@gmail.com>
2021-01-04 18:11:09 -0700

    Added PadLeft and PadRight to EnumerableExtensions

--- src/Gsemac.Collections/Extensions/EnumerableExtensions.cs
+++ src/Gsemac.Collections/Extensions/EnumerableExtensions.cs
@@ -11,6 +11,29 @@ namespace Gsemac.Collections.Extensions {
             return source.Skip(Math.Max(0, source.Count() - length));
 
         }
+        public static IEnumerable<T> PadLeft<T>(this IEnumerable<T> source, int length) {
+
+            int diff = length - source.Count();
+
+            for (int i = 0; i < diff; ++i)
+                yield return default;
+
+            foreach (T value in source)
+                yield return value;
+
+        }
+        public static IEnumerable<T> PadRight<T>(this IEnumerable<T> source, int length) {
+
+            int diff = length - source.Count();
+
+            foreach (T value in source)
+                yield return value;
+
+            for (int i = 0; i < diff; ++i)
+                yield return default;
+
+        }
+
         public static int IndexOf<T>(this IEnumerable<T> source, T value) {
 
             int index = 0;
