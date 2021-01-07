1f859f1f0de55c88b42ea927a7b7f12d6f900db8
gsemac <gabe.semac@gmail.com>
2021-01-07 01:30:44 -0700

    Renamed ArchiveReaderExtensions to ArchiveDecoderExtensions

--- src/Gsemac.IO.Compression/Archive.cs
+++ src/Gsemac.IO.Compression/Archive.cs
@@ -10,7 +10,7 @@ namespace Gsemac.IO.Compression {
 
         public static IArchive OpenFile(string filePath, FileAccess fileAccess = FileAccess.ReadWrite, IArchiveOptions options = null) {
 
-            return CompressionPluginLoader.GetArchiveDecoders().First().OpenFile(filePath, fileAccess, options);
+            return CompressionPluginLoader.GetArchiveDecoders().First().DecodeFile(filePath, fileAccess, options);
 
         }
         public static IArchive OpenStream(Stream stream, FileAccess fileAccess = FileAccess.ReadWrite, bool leaveOpen = false, IArchiveOptions options = null) {
--- src/Gsemac.IO.Compression/Extensions/ArchiveDecoderExtensions.cs
+++ src/Gsemac.IO.Compression/Extensions/ArchiveDecoderExtensions.cs
@@ -2,9 +2,9 @@
 
 namespace Gsemac.IO.Compression.Extensions {
 
-    public static class ArchiveReaderExtensions {
+    public static class ArchiveDecoderExtensions {
 
-        public static IArchive OpenFile(this IArchiveDecoder archiveReader, string filePath, FileAccess fileAccess = FileAccess.ReadWrite, IArchiveOptions options = null) {
+        public static IArchive DecodeFile(this IArchiveDecoder archiveReader, string filePath, FileAccess fileAccess = FileAccess.ReadWrite, IArchiveOptions options = null) {
 
             return archiveReader.Decode(new FileStream(filePath, FileMode.OpenOrCreate, fileAccess), fileAccess, leaveOpen: false, options);
 
