2e1e136a37ef0ae37711303c02c5e1c11e0e8ec5
gsemac <gabe.semac@gmail.com>
2021-01-11 16:00:04 -0700

    Added Run methods to Task class

--- src/Gsemac.Polyfills/System/Threading/Tasks/Task.cs
+++ src/Gsemac.Polyfills/System/Threading/Tasks/Task.cs
@@ -2,15 +2,13 @@
 using System.Threading;
 using System.Threading.Tasks;
 
-using SystemTask = System.Threading.Tasks.Task;
-
 namespace Gsemac.Polyfills.System.Threading.Tasks {
 
     public static class Task {
 
         // .NET Framework 4.0 an earlier do not have Task.Delay defined
 
-        public static SystemTask Delay(int millisecondsDelay, CancellationToken cancellationToken) {
+        public static global::System.Threading.Tasks.Task Delay(int millisecondsDelay, CancellationToken cancellationToken) {
 
             TaskCompletionSource<bool> taskCompletionSource = new TaskCompletionSource<bool>();
             Timer timer = null;
@@ -35,19 +33,56 @@ namespace Gsemac.Polyfills.System.Threading.Tasks {
             return taskCompletionSource.Task;
 
         }
-        public static SystemTask Delay(TimeSpan delay, CancellationToken cancellationToken) {
+        public static global::System.Threading.Tasks.Task Delay(TimeSpan delay, CancellationToken cancellationToken) {
 
             return Delay((int)delay.TotalMilliseconds, cancellationToken);
 
         }
-        public static SystemTask Delay(int millisecondsDelay) {
+        public static global::System.Threading.Tasks.Task Delay(int millisecondsDelay) {
 
             return Delay(millisecondsDelay, CancellationToken.None);
 
         }
-        public static SystemTask Delay(TimeSpan delay) {
+        public static global::System.Threading.Tasks.Task Delay(TimeSpan delay) {
 
             return Delay(delay, CancellationToken.None);
+
+        }
+
+        public static global::System.Threading.Tasks.Task Run(Action action) {
+
+            return Run(action, CancellationToken.None);
+
+        }
+        public static global::System.Threading.Tasks.Task Run(Func<global::System.Threading.Tasks.Task> function) {
+
+            return Run(function, CancellationToken.None);
+
+        }
+        public static global::System.Threading.Tasks.Task Run(Action action, CancellationToken cancellationToken) {
+
+            // Task.Run has some subtle differences compared to Task.Factory.StartNew.
+            // https://stackoverflow.com/a/55949173 (Mykhailo Seniutovych)
+
+            // TaskCreationOptions.DenyChildAttach is replaced with TaskCreationOptions.None because TaskCreationOptions.DenyChildAttach is not available in .NET Framework 4.0.
+            // https://stackoverflow.com/a/26027328 (i3arnon)
+
+            return global::System.Threading.Tasks.Task.Factory.StartNew(action, cancellationToken, TaskCreationOptions.None, TaskScheduler.Default);
+
+        }
+        public static global::System.Threading.Tasks.Task Run(Func<global::System.Threading.Tasks.Task> function, CancellationToken cancellationToken) {
+
+            return global::System.Threading.Tasks.Task.Factory.StartNew(function, cancellationToken, TaskCreationOptions.None, TaskScheduler.Default);
+
+        }
+        public static Task<TResult> Run<TResult>(Func<TResult> function, CancellationToken cancellationToken) {
+
+            return global::System.Threading.Tasks.Task.Factory.StartNew(function, cancellationToken, TaskCreationOptions.None, TaskScheduler.Default);
+
+        }
+        public static Task<TResult> Run<TResult>(Func<TResult> function) {
+
+            return Run(function, CancellationToken.None);
 
         }
 
