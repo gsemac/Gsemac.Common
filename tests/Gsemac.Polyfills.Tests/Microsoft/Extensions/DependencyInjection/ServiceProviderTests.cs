3d696e596b4a57509a90ea2c0e2c2c0c5fc9ba49
gsemac <gabe.semac@gmail.com>
2021-01-11 14:08:03 -0700

    Added Gsemac.Polyfills.Tests project

---
+++ tests/Gsemac.Polyfills.Tests/Microsoft/Extensions/DependencyInjection/ServiceProviderTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Gsemac.Polyfills.Microsoft.Extensions.DependencyInjection.Tests {
+
+    [TestClass]
+    public class ServiceProviderTests {
+
+        // GetService
+
+        [TestMethod]
+        public void TestGetServiceWithNoDependencies() {
+
+            IServiceProvider serviceProvider = new ServiceCollection()
+                .AddTransient<MyServiceWithNoDependencies>()
+                .BuildServiceProvider();
+
+            Assert.IsTrue(serviceProvider.GetService<MyServiceWithNoDependencies>() is MyServiceWithNoDependencies);
+
+        }
+        [TestMethod]
+        public void TestGetServiceWithDependenciesWhenDependenciesAreRegistered() {
+
+            IServiceProvider serviceProvider = new ServiceCollection()
+                .AddTransient<MyServiceWithDependencies>()
+                .AddTransient<MyServiceWithNoDependencies>()
+                .BuildServiceProvider();
+
+            Assert.IsTrue(serviceProvider.GetService<MyServiceWithDependencies>() is MyServiceWithDependencies);
+
+        }
+        [TestMethod]
+        public void TestGetServiceWithDependenciesWhenDependenciesAreNotRegistered() {
+
+            IServiceProvider serviceProvider = new ServiceCollection()
+                .AddTransient<MyServiceWithDependencies>()
+                .BuildServiceProvider();
+
+            Assert.ThrowsException<InvalidOperationException>(() => serviceProvider.GetService<MyServiceWithDependencies>());
+
+        }
+        [TestMethod]
+        public void TestGetServiceWhenServiceIsNotRegistered() {
+
+            IServiceProvider serviceProvider = new ServiceCollection()
+                .BuildServiceProvider();
+
+            Assert.IsTrue(serviceProvider.GetService<MyServiceWithNoDependencies>() is null);
+
+        }
+        [TestMethod]
+        public void TestGetServiceByInterface() {
+
+            IServiceProvider serviceProvider = new ServiceCollection()
+                .AddTransient<IMyService, MyServiceWithNoDependencies>()
+                .BuildServiceProvider();
+
+            Assert.IsTrue(serviceProvider.GetService<IMyService>() is MyServiceWithNoDependencies);
+
+        }
+
+        // GetRequiredService
+
+        [TestMethod]
+        public void TestGetRequiredServiceWhenServiceIsNotRegistered() {
+
+            IServiceProvider serviceProvider = new ServiceCollection()
+                .BuildServiceProvider();
+
+            Assert.ThrowsException<InvalidOperationException>(() => serviceProvider.GetRequiredService<MyServiceWithDependencies>() is MyServiceWithDependencies);
+
+        }
+
+    }
+
+}
---
+++ tests/Gsemac.Polyfills.Tests/Microsoft/Extensions/DependencyInjection/TestClasses.cs
@@ -0,0 +1,36 @@
+﻿namespace Gsemac.Polyfills.Microsoft.Extensions.DependencyInjection.Tests {
+
+    internal interface IMyService { }
+
+    internal class MyServiceWithNoDependencies :
+        IMyService {
+
+        public MyServiceWithNoDependencies() { }
+
+    }
+
+    internal class MyServiceWithDependencies :
+        IMyService {
+
+        public MyServiceWithDependencies(MyServiceWithNoDependencies _) { }
+
+    }
+
+    internal class MyServiceWithMultipleConstructors :
+       IMyService {
+
+        public MyServiceWithMultipleConstructors(MyServiceWithNoDependencies service) { }
+        public MyServiceWithMultipleConstructors(int x, int y) { }
+
+    }
+
+    internal class MyServiceWithMultipleConstructorsWithActivatorUtilitiesConstructor :
+       IMyService {
+
+        [ActivatorUtilitiesConstructor]
+        public MyServiceWithMultipleConstructorsWithActivatorUtilitiesConstructor(MyServiceWithNoDependencies service) { }
+        public MyServiceWithMultipleConstructorsWithActivatorUtilitiesConstructor(int x, int y) { }
+
+    }
+
+}
