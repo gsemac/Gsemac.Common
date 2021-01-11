cde052e38ba1b4c1bcbaec28c3c71ad200c5ff99
gsemac <gabe.semac@gmail.com>
2021-01-11 14:54:16 -0700

    Added additional unit tests

--- tests/Gsemac.Polyfills.Tests/Microsoft/Extensions/DependencyInjection/ServiceProviderTests.cs
+++ tests/Gsemac.Polyfills.Tests/Microsoft/Extensions/DependencyInjection/ServiceProviderTests.cs
@@ -59,6 +59,66 @@ namespace Gsemac.Polyfills.Microsoft.Extensions.DependencyInjection.Tests {
 
         }
 
+        [TestMethod]
+        public void TestGetSingletonService() {
+
+            IServiceProvider serviceProvider = new ServiceCollection()
+                .AddSingleton<IMyService, MyServiceWithNoDependencies>()
+                .BuildServiceProvider();
+
+            Assert.IsTrue(serviceProvider.GetService<IMyService>().Equals(serviceProvider.GetService<IMyService>()));
+
+        }
+        [TestMethod]
+        public void TestGetTransientService() {
+
+            IServiceProvider serviceProvider = new ServiceCollection()
+                .AddTransient<IMyService, MyServiceWithNoDependencies>()
+                .BuildServiceProvider();
+
+            Assert.IsFalse(serviceProvider.GetService<IMyService>().Equals(serviceProvider.GetService<IMyService>()));
+
+        }
+        [TestMethod]
+        public void TestGetScopedService() {
+
+            IServiceProvider serviceProvider = new ServiceCollection()
+                .AddScoped<IMyService, MyServiceWithNoDependencies>()
+                .BuildServiceProvider();
+
+            IMyService rootScopedService = serviceProvider.GetService<IMyService>();
+
+            using (IServiceScope scope = serviceProvider.CreateScope()) {
+
+                IMyService scopedService = scope.ServiceProvider.GetService<IMyService>();
+
+                Assert.IsTrue(scopedService.Equals(scope.ServiceProvider.GetService<IMyService>()));
+                Assert.IsFalse(rootScopedService.Equals(scopedService));
+
+            }
+
+        }
+        [TestMethod]
+        public void TestGetScopedServiceFromRootServiceProvider() {
+
+            IServiceProvider serviceProvider = new ServiceCollection()
+                .AddScoped<IMyService, MyServiceWithNoDependencies>()
+                .BuildServiceProvider();
+
+            Assert.IsTrue(serviceProvider.GetService<IMyService>().Equals(serviceProvider.GetService<IMyService>()));
+
+        }
+        [TestMethod]
+        public void TestGetScopedServiceFromRootServiceProviderWithScopeValidation() {
+
+            IServiceProvider serviceProvider = new ServiceCollection()
+                .AddScoped<IMyService, MyServiceWithNoDependencies>()
+                .BuildServiceProvider(new ServiceProviderOptions() { ValidateScopes = true });
+
+            Assert.ThrowsException<InvalidOperationException>(() => serviceProvider.GetService<IMyService>());
+
+        }
+
         // GetRequiredService
 
         [TestMethod]
