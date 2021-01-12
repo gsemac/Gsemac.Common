f97e7e62281d88f8a66249571873552cde119e2b
gsemac <gabe.semac@gmail.com>
2021-01-12 01:15:42 -0700

    Added IServiceProviderFactory interface

---
+++ src/Gsemac.Polyfills/Microsoft/Extensions/DependencyInjection/DefaultServiceProviderFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Gsemac.Polyfills.Microsoft.Extensions.DependencyInjection {
+
+    public class DefaultServiceProviderFactory :
+        IServiceProviderFactory<IServiceCollection> {
+
+        // Public members
+
+        public DefaultServiceProviderFactory() :
+            this(new ServiceProviderOptions()) {
+
+        }
+        public DefaultServiceProviderFactory(ServiceProviderOptions options) {
+
+            this.options = options;
+
+        }
+
+        public IServiceCollection CreateBuilder(IServiceCollection services) {
+
+            return services;
+
+        }
+        public IServiceProvider CreateServiceProvider(IServiceCollection containerBuilder) {
+
+            return new ServiceProvider(containerBuilder, options);
+
+        }
+
+        // Private members
+
+        private readonly ServiceProviderOptions options;
+
+    }
+
+}
---
+++ src/Gsemac.Polyfills/Microsoft/Extensions/DependencyInjection/IServiceProviderFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gsemac.Polyfills.Microsoft.Extensions.DependencyInjection {
+
+    public interface IServiceProviderFactory<TContainerBuilder> {
+
+        TContainerBuilder CreateBuilder(IServiceCollection services);
+        IServiceProvider CreateServiceProvider(TContainerBuilder containerBuilder);
+
+    }
+
+}
