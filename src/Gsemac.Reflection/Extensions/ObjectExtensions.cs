813591fc54e8d67f6b00072f43e43848e8ae793e
gsemac <gabe.semac@gmail.com>
2021-01-13 23:23:47 -0700

    Added TryCast method to ObjectExtensions

--- src/Gsemac.Reflection/Extensions/ObjectExtensions.cs
+++ src/Gsemac.Reflection/Extensions/ObjectExtensions.cs
@@ -1,10 +1,46 @@
-﻿namespace Gsemac.Reflection.Extensions {
+﻿using System;
+
+namespace Gsemac.Reflection.Extensions {
 
     public static class ObjectExtensions {
 
-        public static IObjectPropertyDictionary ToDictionary(this object obj) {
+        public static IObjectPropertyDictionary ToDictionary(this object obj, ObjectPropertyDictionaryOptions options = ObjectPropertyDictionaryOptions.Default) {
 
-            return new ObjectPropertyDictionary(obj);
+            return new ObjectPropertyDictionary(obj, options);
+
+        }
+
+        public static bool TryCast<T>(this object obj, out T result) {
+
+            if (TryCast(obj, typeof(T), out object resultObject)) {
+
+                result = (T)resultObject;
+
+                return true;
+
+            }
+
+            result = default;
+
+            return false;
+
+        }
+        public static bool TryCast(this object obj, Type type, out object result) {
+
+            try {
+
+                result = Convert.ChangeType(obj, type);
+
+                return true;
+
+            }
+            catch (InvalidCastException) {
+
+                result = default;
+
+                return false;
+
+            }
 
         }
 
