ec5427571588063c7b297648aff3b69c2b6a2634
gsemac <gabe.semac@gmail.com>
2021-01-17 14:09:23 -0700

    Added TrySetSecurityProtocolsEnabled methods

--- src/Gsemac.Net/NetUtilities.cs
+++ src/Gsemac.Net/NetUtilities.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gsemac.Polyfills.System.Net;
+using System;
 using System.Net.Security;
 using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
@@ -71,6 +72,49 @@ namespace Gsemac.Net {
                 }
 
             }
+
+        }
+
+        public static bool TrySetSecurityProtocolsEnabled(SecurityProtocolType securityProtocols, bool enabled) {
+
+            return TrySetSecurityProtocolsEnabled((System.Net.SecurityProtocolType)securityProtocols, enabled);
+
+        }
+        public static bool TrySetSecurityProtocolsEnabled(System.Net.SecurityProtocolType securityProtocols, bool enabled) {
+
+            try {
+
+                if (enabled)
+                    System.Net.ServicePointManager.SecurityProtocol |= securityProtocols;
+                else
+                    System.Net.ServicePointManager.SecurityProtocol &= ~securityProtocols;
+
+                return true;
+
+            }
+            catch (Exception) {
+
+                return false;
+
+            }
+
+        }
+
+        public static bool TrySetNet40SecurityProtocolsEnabled(bool enabled) {
+
+            return TrySetSecurityProtocolsEnabled(SecurityProtocolType.Ssl3, enabled) &&
+               TrySetSecurityProtocolsEnabled(SecurityProtocolType.Tls, enabled);
+
+        }
+        public static bool TrySetNet45SecurityProtocolsEnabled(bool enabled) {
+
+            return TrySetSecurityProtocolsEnabled(SecurityProtocolType.Tls11, enabled) &&
+                TrySetSecurityProtocolsEnabled(SecurityProtocolType.Tls12, enabled);
+
+        }
+        public static bool TrySetNet48SecurityProtocolsEnabled(bool enabled) {
+
+            return TrySetSecurityProtocolsEnabled(SecurityProtocolType.Tls13, enabled);
 
         }
 
