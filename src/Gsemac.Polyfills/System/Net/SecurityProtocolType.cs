8813e5b4981bb52a614d69bd3c852b95d39c881c
gsemac <gabe.semac@gmail.com>
2021-01-17 14:08:31 -0700

    Added SecurityProtocolType enum

---
+++ src/Gsemac.Polyfills/System/Net/SecurityProtocolType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gsemac.Polyfills.System.Net {
+
+    [Flags]
+    public enum SecurityProtocolType {
+        SystemDefault = 0,
+        Ssl3 = 48,
+        Tls = 192,
+        Tls11 = 768,
+        Tls12 = 3072,
+        Tls13 = 12288,
+    }
+
+}
