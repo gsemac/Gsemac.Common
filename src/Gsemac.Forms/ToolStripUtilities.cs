430df58ba7abc91f0976456c324bf99ed1d40ff3
gsemac <gabe.semac@gmail.com>
2021-01-17 13:15:57 -0700

    Added ToolStripUtilities class

---
+++ src/Gsemac.Forms/ToolStripUtilities.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace Gsemac.Forms {
+
+    public static class ToolStripUtilities {
+
+        public static void SetRoundedEdgesEnabled(ToolStrip control, bool roundedEdges) {
+
+            if (control.Renderer is ToolStripProfessionalRenderer professionalRenderer)
+                professionalRenderer.RoundedEdges = roundedEdges;
+
+        }
+
+    }
+
+}
