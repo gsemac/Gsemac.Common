80b4de5816443bb59de1fc408afe732c1154d5f0
gsemac <gabe.semac@gmail.com>
2021-01-21 15:31:37 -0700

    Added Base64 class

---
+++ src/Gsemac.Text/Base64.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Gsemac.Text {
+
+    public static class Base64 {
+
+        // Public members
+
+        public static string EncodeString(string input) {
+
+            return EncodeString(input, Encoding.UTF8);
+
+        }
+        public static string DecodeString(string input) {
+
+            return DecodeString(input, Encoding.UTF8);
+
+        }
+        public static string EncodeString(string input, Encoding encoding) {
+
+            return Convert.ToBase64String(encoding.GetBytes(input));
+
+        }
+        public static string DecodeString(string input, Encoding encoding) {
+
+            // The default FromBase64String method will throw an exception if the input string is not padded.
+
+            input = PadBase64String(input);
+
+            return encoding.GetString(Convert.FromBase64String(input));
+
+        }
+
+        public static bool IsBase64String(string input) {
+
+            // Slightly modified from the answer given here:
+            // https://stackoverflow.com/a/54143400 (Tomas Kubes)
+
+            if (string.IsNullOrWhiteSpace(input))
+                return false;
+
+            input = PadBase64String(input.Trim());
+
+            return (input.Length % 4 == 0) &&
+                Regex.IsMatch(input, @"^[a-z0-9\+/]*={0,3}$", RegexOptions.IgnoreCase);
+
+        }
+
+        // Private members
+
+        private static string PadBase64String(string input) {
+
+            if (string.IsNullOrWhiteSpace(input))
+                return input;
+
+            if (input.Length % 4 > 0)
+                input = input.PadRight(input.Length + 4 - input.Length % 4, '=');
+
+            return input;
+
+        }
+
+    }
+
+}
---
+++ tests/Gsemac.Text.Tests/Base64Tests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gsemac.Text.Tests {
+
+    [TestClass]
+    public class Base64Tests {
+
+        // IsBase64String
+
+        [TestMethod]
+        public void TestIsBase64WithPaddedBase64String() {
+
+            Assert.IsTrue(Base64.IsBase64String("aGVsbG8gd29ybGQ="));
+
+        }
+        [TestMethod]
+        public void TestIsBase64WithUnpaddedBase64String() {
+
+            Assert.IsTrue(Base64.IsBase64String("aGVsbG8gd29ybGQ"));
+
+        }
+        [TestMethod]
+        public void TestIsBase64WithUppercaseBase64String() {
+
+            Assert.IsTrue(Base64.IsBase64String("AGVSBG8GD29YBGQ="));
+
+        }
+        [TestMethod]
+        public void TestIsBase64WithExcessivelyPaddedBase64String() {
+
+            Assert.IsFalse(Base64.IsBase64String("aGVsbG8gd29ybGQ======"));
+
+        }
+        [TestMethod]
+        public void TestIsBase64WithInvalidBase64String() {
+
+            Assert.IsFalse(Base64.IsBase64String("aGVsbG8gd29ybGQ{"));
+
+        }
+        [TestMethod]
+        public void TestIsBase64WithEmptyString() {
+
+            Assert.IsFalse(Base64.IsBase64String(string.Empty));
+
+        }
+
+    }
+
+}
