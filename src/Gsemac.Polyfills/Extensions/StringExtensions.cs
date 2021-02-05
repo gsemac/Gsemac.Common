73cd432376de0762e2ad75b2b4286e419e785441
gsemac <gabe.semac@gmail.com>
2021-02-04 23:00:24 -0700

    Added Contains methods to StringExtensions

---
+++ src/Gsemac.Polyfills/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Gsemac.Polyfills.Extensions {
+
+    public static class StringExtensions {
+
+        public static bool Contains(this string str, char value, StringComparison comparisonType) {
+
+            return str.Contains(value.ToString(), comparisonType);
+
+        }
+        public static bool Contains(this string str, string value, StringComparison comparisonType) {
+
+            return str.IndexOf(value, comparisonType) >= 0;
+
+        }
+
+    }
+
+}
