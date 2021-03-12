ff3dc3091f41c832781a4f05678dcb5771b6b319
gsemac <gabe.semac@gmail.com>
2021-03-11 23:52:23 -0700

    Added Insert and InsertRange to CollectionExtensions

--- src/Gsemac.Collections/Extensions/CollectionExtensions.cs
+++ src/Gsemac.Collections/Extensions/CollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Gsemac.Collections.Extensions {
 
@@ -6,7 +8,7 @@ namespace Gsemac.Collections.Extensions {
 
         public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items) {
 
-            if (collection is IList<T> list) {
+            if (collection is List<T> list) {
 
                 list.AddRange(items);
 
@@ -15,6 +17,45 @@ namespace Gsemac.Collections.Extensions {
 
                 foreach (T item in items)
                     collection.Add(item);
+
+            }
+
+        }
+        public static void Insert<T>(this ICollection<T> collection, int index, T item) {
+
+            if (collection is IList<T> list) {
+
+                list.Insert(index, item);
+
+            }
+            else {
+
+                collection.InsertRange(index, new[] { item });
+
+            }
+
+        }
+        public static void InsertRange<T>(this ICollection<T> collection, int index, IEnumerable<T> items) {
+
+            if (index < 0 || index > collection.Count)
+                throw new ArgumentOutOfRangeException(nameof(index), "Index was out of range. Must be non-negative and less than the size of the collection.");
+
+            if (collection is List<T> list) {
+
+                list.InsertRange(index, items);
+
+            }
+            else {
+
+                // I know this is bad
+
+                List<T> temp = new List<T>(collection);
+
+                collection.Clear();
+
+                collection.AddRange(temp.Take(index));
+                collection.AddRange(items);
+                collection.AddRange(temp.Skip(index));
 
             }
 
