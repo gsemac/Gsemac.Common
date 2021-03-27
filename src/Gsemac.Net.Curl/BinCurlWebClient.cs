cc3e12271a7cbcdda23925bf99ed6b16d0f8eccf
gsemac <gabe.semac@gmail.com>
2021-03-26 20:43:42 -0600

    Added additional constructors to CurlWebClient and BinCurlWebClient

--- src/Gsemac.Net.Curl/BinCurlWebClient.cs
+++ src/Gsemac.Net.Curl/BinCurlWebClient.cs
@@ -1,8 +1,5 @@
 ﻿namespace Gsemac.Net.Curl {
 
-    /// <summary>
-    /// Provides common methods for sending data to and receiving data via Curl from a resource identified by a URI.
-    /// </summary>
     public class BinCurlWebClient :
         WebClientBase {
 
@@ -11,12 +8,11 @@
         public BinCurlWebClient() :
             this(CurlWebRequestOptions.Default) {
         }
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BinCurlWebClient"/> class.
-        /// </summary>
-        /// <param name="curlExecutablePath">Path to Curl executable.</param>
         public BinCurlWebClient(ICurlWebRequestOptions curlOptions) :
             base(new BinCurlHttpWebRequestFactory(curlOptions)) {
+        }
+        public BinCurlWebClient(BinCurlHttpWebRequestFactory webRequestFactory) :
+            base(webRequestFactory) {
         }
 
     }
--- src/Gsemac.Net.Curl/CurlWebClient.cs
+++ src/Gsemac.Net.Curl/CurlWebClient.cs
@@ -1,18 +1,18 @@
 ﻿namespace Gsemac.Net.Curl {
 
-    /// <summary>
-    /// Provides common methods for sending data to and receiving data via Curl from a resource identified by a URI.
-    /// </summary>
     public class CurlWebClient :
         WebClientBase {
 
         // Public members
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CurlWebClient"/> class.
-        /// </summary>
         public CurlWebClient() :
-            base(new CurlHttpWebRequestFactory()) {
+            this(CurlWebRequestOptions.Default) {
+        }
+        public CurlWebClient(ICurlWebRequestOptions curlOptions) :
+           base(new CurlHttpWebRequestFactory(curlOptions)) {
+        }
+        public CurlWebClient(CurlHttpWebRequestFactory webRequestFactory) :
+            base(webRequestFactory) {
         }
 
     }
