f5326dbdf4c4208308c01e8b6102cfb4959aaffb
gsemac <gabe.semac@gmail.com>
2021-03-26 21:19:59 -0600

    Added ClipboardUtilities class

---
+++ src/Gsemac.Forms/ClipboardUtilities.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows.Forms;
+
+namespace Gsemac.Forms {
+
+    public static class ClipboardUtilities {
+
+        // Public members
+
+        public static string GetClipboardText() {
+
+            string result = string.Empty;
+
+            ThreadUtilities.DoInStaThread(() => {
+
+                if (Clipboard.GetDataObject() is DataObject clipboardData) {
+
+                    result = GetClipboardTextOrHtml(clipboardData);
+
+                }
+
+            });
+
+            return result;
+
+        }
+
+        // Private members
+
+        private static string GetClipboardTextOrHtml(DataObject clipboardData) {
+
+            string result = string.Empty;
+
+            if (clipboardData.ContainsText(TextDataFormat.Html)) {
+
+                result = clipboardData.GetText(TextDataFormat.Html);
+
+            }
+            else if (clipboardData.ContainsText(TextDataFormat.Html)) {
+
+                result = clipboardData.GetText(TextDataFormat.Text);
+
+            }
+
+            return result;
+
+        }
+
+    }
+
+}
---
+++ src/Gsemac.Forms/ThreadUtilities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Gsemac.Forms {
+
+    internal static class ThreadUtilities {
+
+        public static void DoInStaThread(Action action) {
+
+            if (Thread.CurrentThread.GetApartmentState() == ApartmentState.STA) {
+
+                action();
+
+            }
+            else {
+
+                Thread thread = new Thread(() => action());
+
+                thread.SetApartmentState(ApartmentState.STA);
+
+                thread.Start();
+                thread.Join();
+
+            }
+
+        }
+
+    }
+
+}
