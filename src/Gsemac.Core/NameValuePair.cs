c087bf56de123695bd2fc7e58e2f74f3292ce47a
gsemac <gabe.semac@gmail.com>
2021-04-23 22:10:31 -0600

    Added NameValuePair class

---
+++ src/Gsemac.Core/NameValuePair.cs
@@ -0,0 +1,17 @@
+﻿namespace Gsemac.Core {
+
+    public sealed class NameValuePair {
+
+        public string Name { get; }
+        public string Value { get; }
+
+        public NameValuePair(string name, string value) {
+
+            this.Name = name;
+            this.Value = value;
+
+        }
+
+    }
+
+}
