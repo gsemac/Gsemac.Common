3f2496c396e6e9cf8621833a92384fcfce455156
gsemac <gabe.semac@gmail.com>
2021-04-27 16:26:31 -0600

    Added unit tests for GetDomainName

--- tests/Gsemac.Net.Tests/UrlTests.cs
+++ tests/Gsemac.Net.Tests/UrlTests.cs
@@ -156,6 +156,26 @@ namespace Gsemac.Net.Tests {
             Assert.AreEqual("stackoverflow.com:443", new Url("https://stackoverflow.com:443/").Host);
 
         }
+        [TestMethod]
+        public void TestToStringAfterSetHost() {
+
+            IUrl url = new Url("https://stackoverflow.com:443/") {
+                Host = "stackexchange.com",
+            };
+
+            Assert.AreEqual("https://stackexchange.com/", url.ToString());
+
+        }
+        [TestMethod]
+        public void TestToStringAfterSetHostWithPort() {
+
+            IUrl url = new Url("https://stackoverflow.com:443/") {
+                Host = "stackexchange.com:8080",
+            };
+
+            Assert.AreEqual("https://stackexchange.com:8080/", url.ToString());
+
+        }
 
         // Hostname
 
@@ -175,6 +195,107 @@ namespace Gsemac.Net.Tests {
         public void TestGetHostnameWithPort() {
 
             Assert.AreEqual("stackoverflow.com", new Url("https://stackoverflow.com:443/").Hostname);
+
+        }
+        [TestMethod]
+        public void TestGetHostnameWithFullyQualifiedDomainName() {
+
+            Assert.AreEqual("stackoverflow.com.", new Url("https://stackoverflow.com./").Hostname);
+
+        }
+        [TestMethod]
+        public void TestToStringAfterSetHostname() {
+
+            IUrl url = new Url("https://stackoverflow.com:443/") {
+                Hostname = "stackexchange.com",
+            };
+
+            Assert.AreEqual("https://stackexchange.com:443/", url.ToString());
+
+        }
+
+        // GetDomainName
+
+        [TestMethod]
+        public void TestGetDomainNameWithSubdomain() {
+
+            Url url = new Url("https://www.stackoverflow.com/");
+
+            Assert.AreEqual("stackoverflow.com", Url.GetDomainName(url.ToString()));
+
+        }
+        [TestMethod]
+        public void TestGetDomainNameWithoutProtocol() {
+
+            Url url = new Url("www.stackoverflow.com");
+
+            Assert.AreEqual("stackoverflow.com", Url.GetDomainName(url.ToString()));
+
+        }
+        [TestMethod]
+        public void TestGetDomainNameWithPath() {
+
+            Url url = new Url("https://codegolf.stackexchange.com/questions/198550/simple-circular-words");
+
+            Assert.AreEqual("stackexchange.com", Url.GetDomainName(url.ToString()));
+
+        }
+        [TestMethod]
+        public void TestGetDomainNameWithoutSubdomain() {
+
+            Url url = new Url("https://stackoverflow.com/");
+
+            Assert.AreEqual("stackoverflow.com", Url.GetDomainName(url.ToString()));
+
+        }
+        [TestMethod]
+        public void TestGetDomainNameWithMultipartTld() {
+
+            Url url = new Url("https://website.co.jp/");
+
+            Assert.AreEqual("website.co.jp", Url.GetDomainName(url.ToString()));
+
+        }
+        [TestMethod]
+        public void TestGetDomainNameEndingWithMultipartTld() {
+
+            // "websiteco.jp" ends with a multipart TLD ("co.jp"), but this should not be detected as such (because there is no dot separator).
+
+            Url url = new Url("https://websiteco.jp/");
+
+            Assert.AreEqual("websiteco.jp", Url.GetDomainName(url.ToString()));
+
+        }
+        [TestMethod]
+        public void TestGetDomainNameWithSubdomainAndMultipartTld() {
+
+            Url url = new Url("https://subdomain.website.co.jp/");
+
+            Assert.AreEqual("website.co.jp", Url.GetDomainName(url.ToString()));
+
+        }
+        [TestMethod]
+        public void TestGetDomainNameWithIpAddress() {
+
+            Url url = new Url("https://192.168.1.1/");
+
+            Assert.AreEqual("192.168.1.1", Url.GetDomainName(url.ToString()));
+
+        }
+        [TestMethod]
+        public void TestGetDomainNameWithFullyQualifiedDomainName() {
+
+            Url url = new Url("https://stackoverflow.com./");
+
+            Assert.AreEqual("stackoverflow.com", Url.GetDomainName(url.ToString()));
+
+        }
+        [TestMethod]
+        public void TestGetDomainNameDomainName() {
+
+            Url url = new Url("stackoverflow.com");
+
+            Assert.AreEqual("stackoverflow.com", Url.GetDomainName(url.ToString()));
 
         }
 
