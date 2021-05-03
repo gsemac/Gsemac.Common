2470da08623b10dc4c7338b9e1782b8d2cf0913c
gsemac <gabe.semac@gmail.com>
2021-05-03 02:40:30 -0600

    Added CreateWebClient method to HttpWebRequestFactoryExtensions

--- src/Gsemac.Net/Extensions/HttpWebRequestFactoryExtensions.cs
+++ src/Gsemac.Net/Extensions/HttpWebRequestFactoryExtensions.cs
@@ -1,10 +1,17 @@
-﻿namespace Gsemac.Net.Extensions {
+﻿using System.Net;
+
+namespace Gsemac.Net.Extensions {
 
     public static class HttpWebRequestFactoryExtensions {
 
         public static IWebClientFactory ToWebClientFactory(this IHttpWebRequestFactory webRequestFactory) {
 
             return new WebClientFactory(webRequestFactory);
+
+        }
+        public static WebClient CreateWebClient(this IHttpWebRequestFactory webRequestFactory) {
+
+            return webRequestFactory.ToWebClientFactory().Create();
 
         }
 
