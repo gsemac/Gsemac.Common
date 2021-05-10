dfd4b47ba17421fbfbd3a5a59700a99fdac0ef79
gsemac <gabe.semac@gmail.com>
2021-05-10 02:10:29 -0600

    Made ToArray method more consistent across different types of streams

--- src/Gsemac.IO/Extensions/StreamExtensions.cs
+++ src/Gsemac.IO/Extensions/StreamExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace Gsemac.IO.Extensions {
 
@@ -6,22 +7,46 @@ namespace Gsemac.IO.Extensions {
 
         public static byte[] ToArray(this Stream stream) {
 
+            if (stream is null)
+                throw new ArgumentNullException(nameof(stream));
+
+            byte[] result;
+            long currentPos = -1;
+
             if (stream is MemoryStream memoryStream) {
 
-                return memoryStream.ToArray();
+                result = memoryStream.ToArray();
 
             }
             else {
 
                 using (var ms = new MemoryStream()) {
 
+                    if (stream.CanSeek) {
+
+                        // Reset the stream's position so that we can read the entire thing.
+
+                        currentPos = stream.Position;
+
+                        if (currentPos != 0)
+                            stream.Seek(0, SeekOrigin.Begin);
+
+                    }
+
                     stream.CopyTo(ms);
 
-                    return ms.ToArray();
+                    result = ms.ToArray();
 
                 }
 
             }
+
+            // Restore the stream's position.
+
+            if (currentPos >= 0)
+                stream.Seek(currentPos, SeekOrigin.Begin);
+
+            return result;
 
         }
 
---
+++ tests/Gsemac.IO.Tests/Extensions/StreamExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using Gsemac.IO.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Gsemac.IO.Tests.Extensions {
+
+    [TestClass]
+    public class StreamExtensionsTests {
+
+        [TestMethod]
+        public void TestToArrayWithNullStream() {
+
+            Assert.ThrowsException<ArgumentNullException>(() => ((Stream)null).ToArray());
+
+        }
+        [TestMethod]
+        public void TestToArrayWithEmptyStream() {
+
+            using (Stream ms = new MemoryStream())
+                Assert.AreEqual(0, ms.ToArray().Length);
+
+        }
+        [TestMethod]
+        public void TestToArrayWithStreamWithPositivePosition() {
+
+            // The entire contents of the stream are returned regardless of its position.
+
+            using (Stream ms = new MemoryStream(Encoding.UTF8.GetBytes("hello world"))) {
+
+                ms.Seek(5, SeekOrigin.Begin);
+
+                Assert.AreEqual("hello world", Encoding.UTF8.GetString(ms.ToArray()));
+
+            }
+
+        }
+
+    }
+
+}
