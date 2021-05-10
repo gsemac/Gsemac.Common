daf968352bf378b2f4609a2b46570b5acd6941ab
gsemac <gabe.semac@gmail.com>
2021-05-10 03:05:48 -0600

    Added unit tests for UnbufferedStreamReader

---
+++ tests/Gsemac.IO.Tests/UnbufferedStreamReaderTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
+
+namespace Gsemac.IO.Tests {
+
+    [TestClass]
+    public class UnbufferedStreamReaderTests {
+
+        [TestMethod]
+        public void TestPeekWithSingleByteCharacter() {
+
+            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes("abc")))
+            using (UnbufferedStreamReader sr = new UnbufferedStreamReader(ms, Encoding.UTF8)) {
+
+                Assert.AreEqual('a'.ToString(), ((char)sr.Peek()).ToString());
+
+            }
+
+        }
+        [TestMethod]
+        public void TestPeekWithMultibyteCharacter() {
+
+            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes("À")))
+            using (UnbufferedStreamReader sr = new UnbufferedStreamReader(ms, Encoding.UTF8)) {
+
+                Assert.AreEqual('À'.ToString(), ((char)sr.Peek()).ToString());
+
+            }
+
+        }
+
+    }
+
+}
