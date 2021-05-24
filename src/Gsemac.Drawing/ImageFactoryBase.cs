e1fd21874b47dd9bd586e79c544042a1a3bdca95
gsemac <gabe.semac@gmail.com>
2021-05-24 16:10:26 -0600

    Added FromStream overload to FileFormatFactoryExtensions

--- src/Gsemac.Drawing/ImageFactoryBase.cs
+++ src/Gsemac.Drawing/ImageFactoryBase.cs
@@ -1,5 +1,6 @@
 ﻿using Gsemac.Drawing.Imaging;
 using Gsemac.IO;
+using Gsemac.IO.Extensions;
 using System.Collections.Generic;
 using System.IO;
 
@@ -18,28 +19,8 @@ namespace Gsemac.Drawing {
 
         public IImage FromStream(Stream stream, IFileFormat imageFormat = null) {
 
-            if (imageFormat is null) {
-
-                // Read the file signature from the stream into a buffer, which is then concatenated with the original stream.
-                // This allows us to read the file signature from streams that don't support seeking.
-
-                const int maxSignatureLength = 64;
-
-                using (MemoryStream fileSignatureBuffer = new MemoryStream(new byte[maxSignatureLength], 0, maxSignatureLength, writable: true, publiclyVisible: true)) {
-
-                    int bytesRead = stream.Read(fileSignatureBuffer.GetBuffer(), 0, maxSignatureLength);
-
-                    fileSignatureBuffer.SetLength(bytesRead);
-
-                    imageFormat = FileFormatFactory.Default.FromStream(fileSignatureBuffer);
-
-                    fileSignatureBuffer.Seek(0, SeekOrigin.Begin);
-
-                    return FromStream(new ConcatStream(fileSignatureBuffer, stream), imageFormat);
-
-                }
-
-            }
+            if (imageFormat is null)
+                stream = FileFormatFactory.Default.FromStream(stream, out imageFormat);
 
             IImageCodec imageCodec = imageCodecFactory.FromFileFormat(imageFormat);
 
--- src/Gsemac.IO/Extensions/FileFormatFactoryExtensions.cs
+++ src/Gsemac.IO/Extensions/FileFormatFactoryExtensions.cs
@@ -1,18 +1,55 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace Gsemac.IO.Extensions {
 
     public static class FileFormatFactoryExtensions {
 
+        public static IFileFormat FromFile(this IFileFormatFactory fileFormatFactory, string filePath) {
+
+            using (Stream stream = File.OpenRead(filePath))
+                return fileFormatFactory.FromStream(stream) ?? fileFormatFactory.FromFileExtension(filePath);
+
+        }
         public static IFileFormat FromMimeType(this IFileFormatFactory fileFormatFactory, string mimeType) {
 
             return fileFormatFactory.FromMimeType(new MimeType(mimeType));
 
         }
-        public static IFileFormat FromFile(this IFileFormatFactory fileFormatFactory, string filePath) {
+        public static Stream FromStream(this IFileFormatFactory fileFormatFactory, Stream stream, out IFileFormat fileFormat) {
 
-            using (Stream stream = File.OpenRead(filePath))
-                return fileFormatFactory.FromStream(stream) ?? fileFormatFactory.FromFileExtension(filePath);
+            // Read the file signature from the stream into a buffer, which is then concatenated with the original stream.
+            // This allows us to read the file signature from streams that don't support seeking.
+
+            int maxSignatureLength = fileFormatFactory.GetKnownFileFormats()
+                .Where(format => format.Signatures is object && format.Signatures.Any())
+                .SelectMany(format => format.Signatures.Select(sig => sig.Length))
+                .OrderByDescending(length => length)
+                .FirstOrDefault();
+
+            MemoryStream fileSignatureBuffer = new MemoryStream(new byte[maxSignatureLength], 0, maxSignatureLength, writable: true, publiclyVisible: true);
+
+            try {
+
+                int bytesRead = stream.Read(fileSignatureBuffer.GetBuffer(), 0, maxSignatureLength);
+
+                fileSignatureBuffer.SetLength(bytesRead);
+
+                fileFormat = fileFormatFactory.FromStream(fileSignatureBuffer);
+
+                fileSignatureBuffer.Seek(0, SeekOrigin.Begin);
+
+                return new ConcatStream(fileSignatureBuffer, stream);
+
+            }
+            catch (Exception) {
+
+                fileSignatureBuffer.Dispose();
+
+                throw;
+
+            }
 
         }
 
