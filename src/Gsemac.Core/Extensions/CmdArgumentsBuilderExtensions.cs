6cbf9db6fcf1892ab9d3ed99d5fbba89db689967
gsemac <gabe.semac@gmail.com>
2021-06-09 02:07:33 -0600

    Added CmdArgumentsBuilderExtensions

---
+++ src/Gsemac.Core/Extensions/CmdArgumentsBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace Gsemac.Core.Extensions {
+
+    public static class CmdArgumentsBuilderExtensions {
+
+        public static ICmdArgumentsBuilder WithArgument(this ICmdArgumentsBuilder argumentsBuilder, string argument) {
+
+            argumentsBuilder.AddArgument(argument);
+
+            return argumentsBuilder;
+
+        }
+        public static ICmdArgumentsBuilder WithArgument(this ICmdArgumentsBuilder argumentsBuilder, string name, string value) {
+
+            argumentsBuilder.AddArgument(name, value);
+
+            return argumentsBuilder;
+
+        }
+        public static ICmdArgumentsBuilder WithArguments(this ICmdArgumentsBuilder argumentsBuilder, params string[] arguments) {
+
+            foreach (string argument in arguments)
+                argumentsBuilder.AddArgument(argument);
+
+            return argumentsBuilder;
+
+        }
+
+    }
+
+}
