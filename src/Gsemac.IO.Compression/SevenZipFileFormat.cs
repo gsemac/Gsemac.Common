3189df1f56bced4e10e296b252093e1d070f2f7e
gsemac <gabe.semac@gmail.com>
2021-06-13 02:31:03 -0600

    Added additional file extension to SevenZipFileFormat

--- src/Gsemac.IO.Compression/SevenZipFileFormat.cs
+++ src/Gsemac.IO.Compression/SevenZipFileFormat.cs
@@ -5,7 +5,7 @@ namespace Gsemac.IO.Compression {
     public class SevenZipFileFormat :
         FileFormatBase {
 
-        public override IEnumerable<string> Extensions => new string[] { ".7z" };
+        public override IEnumerable<string> Extensions => new string[] { ".7z", ".cb7" };
         public override IEnumerable<IFileSignature> Signatures => new IFileSignature[] {
             new FileSignature(0x37, 0x7A, 0xBC, 0xAF, 0x27, 0x1C),
         };
