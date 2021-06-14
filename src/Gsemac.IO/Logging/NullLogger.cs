d6599a9cc0484b337b138cb361e2b67d701781b3
gsemac <gabe.semac@gmail.com>
2021-06-14 12:34:31 -0600

    Added NullLogger class

---
+++ src/Gsemac.IO/Logging/NullLogger.cs
@@ -0,0 +1,15 @@
+﻿namespace Gsemac.IO.Logging {
+
+    public class NullLogger :
+         ILogger {
+
+        public bool Enabled { get; set; }
+        public string Name { get; set; }
+
+        public event LogEventHandler Logged;
+
+        public void Log(ILogMessage message) { }
+
+    }
+
+}
