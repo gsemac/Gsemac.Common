8810009dce7b5b4bd15c9293bc24d3b4e16c5dba
gsemac <gabe.semac@gmail.com>
2021-07-09 12:57:22 -0600

    Added Gdi32 and GdiPlus classes

---
+++ src/Gsemac.Win32/Gdi32.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Gsemac.Win32 {
+
+    public static class Gdi32 {
+
+        // Public members
+
+        public static int MM_ISOTROPIC = 7;
+        public static int MM_ANISOTROPIC = 8;
+
+        public static IntPtr SetMetaFileBitsEx(uint cbBuffer, byte[] lpData) {
+
+            return SetMetaFileBitsExNative(cbBuffer, lpData);
+
+        }
+        public static IntPtr CopyMetaFile(IntPtr hmf, string filename) {
+
+            return CopyMetaFileNative(hmf, filename);
+
+        }
+        public static bool DeleteMetaFile(IntPtr hmf) {
+
+            return DeleteMetaFileNative(hmf);
+
+        }
+        public static bool DeleteEnhMetaFile(IntPtr hmf) {
+
+            return DeleteEnhMetaFileNative(hmf);
+
+        }
+
+        // Private members
+
+        [DllImport("gdi32", EntryPoint = "SetMetaFileBitsEx")]
+        private static extern IntPtr SetMetaFileBitsExNative(uint cbBuffer, byte[] lpData);
+        [DllImport("gdi32", EntryPoint = "CopyMetaFile")]
+        private static extern IntPtr CopyMetaFileNative(IntPtr hmf, string filename);
+        [DllImport("gdi32", EntryPoint = "DeleteMetaFile")]
+        private static extern bool DeleteMetaFileNative(IntPtr hmf);
+        [DllImport("gdi32", EntryPoint = "DeleteEnhMetaFile")]
+        private static extern bool DeleteEnhMetaFileNative(IntPtr hmf);
+
+    }
+
+}
---
+++ src/Gsemac.Win32/GdiPlus.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace Gsemac.Win32 {
+
+    [Flags]
+    [SuppressMessage("Naming", "CA1712:Do not prefix enum values with type name", Justification = "The names are written to be identical to those in gdiplusenums.h.")]
+    public enum EmfToWmfBitsFlags {
+        EmfToWmfBitsFlagsDefault = 0x00000000,
+        EmfToWmfBitsFlagsEmbedEmf = 0x00000001,
+        EmfToWmfBitsFlagsIncludePlaceable = 0x00000002,
+        EmfToWmfBitsFlagsNoXORClip = 0x00000004,
+    }
+
+    public static class GdiPlus {
+
+        // Public members
+
+        public static uint GdipEmfToWmfBits(IntPtr hemf, uint cbData16, byte[] pData16, int iMapMode, EmfToWmfBitsFlags eFlags) {
+
+            return GdipEmfToWmfBitsNative(hemf, cbData16, pData16, iMapMode, eFlags);
+
+        }
+
+        // Private members
+
+        [DllImport("gdiplus", EntryPoint = "GdipEmfToWmfBits")]
+        private static extern uint GdipEmfToWmfBitsNative(IntPtr hemf, uint cbData16, byte[] pData16, int iMapMode, EmfToWmfBitsFlags eFlags);
+
+    }
+
+}
