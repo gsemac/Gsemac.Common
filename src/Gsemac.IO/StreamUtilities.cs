162b7f1291019976e7430ae8c00224ca59f607b9
gsemac <gabe.semac@gmail.com>
2021-11-04 15:36:58 -0600

    Added StreamUtilities class

---
+++ src/Gsemac.IO/StreamUtilities.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Gsemac.IO {
+
+    public static class StreamUtilities {
+
+        // Public members
+
+        public static string StreamToString(Stream stream) {
+
+            return StreamToString(stream, Encoding.UTF8);
+
+        }
+        public static string StreamToString(Stream stream, Encoding encoding) {
+
+            using (StreamReader sr = new StreamReader(stream, encoding))
+                return sr.ReadToEnd();
+
+        }
+
+    }
+
+}
