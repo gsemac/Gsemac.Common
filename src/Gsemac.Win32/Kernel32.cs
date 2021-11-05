f8fb2167059bc3dd73072776de61b24042fcf573
gsemac <gabe.semac@gmail.com>
2021-11-04 21:42:22 -0600

    Added additional methods to Kernel32 Added AllocConsole, AttachConsole, and FreeConsole methods.

--- src/Gsemac.Win32/Kernel32.cs
+++ src/Gsemac.Win32/Kernel32.cs
@@ -6,11 +6,28 @@ namespace Gsemac.Win32 {
 
         // Public members
 
+        public static int ATTACH_PARENT_PROCESS = -1;
+
         public static bool AddDllDirectory(string newDirectory) {
 
             return AddDllDirectoryNative(newDirectory);
 
-        } // Requires Windows 8+, or Windows Vista+ with KB2533623 
+        } // Requires Windows 8+, or Windows Vista+ with KB2533623
+        public static bool AllocConsole() {
+
+            return AllocConsoleNative();
+
+        }
+        public static bool AttachConsole(int dwProcessId) {
+
+            return AttachConsoleNative(dwProcessId);
+
+        }
+        public static bool FreeConsole() {
+
+            return FreeConsoleNative();
+
+        }
         public static bool SetDllDirectory(string lpPathName) {
 
             return SetDllDirectoryNative(lpPathName);
@@ -21,6 +38,12 @@ namespace Gsemac.Win32 {
 
         [DllImport("kernel32", EntryPoint = "AddDllDirectory", CharSet = CharSet.Unicode, SetLastError = true)]
         private static extern bool AddDllDirectoryNative(string newDirectory);
+        [DllImport("kernel32", EntryPoint = "AllocConsole", SetLastError = true)]
+        internal static extern bool AllocConsoleNative();
+        [DllImport("kernel32", EntryPoint = "AttachConsole", SetLastError = true)]
+        private static extern bool AttachConsoleNative(int dwProcessId);
+        [DllImport("kernel32", EntryPoint = "FreeConsole", SetLastError = true)]
+        private static extern bool FreeConsoleNative();
         [DllImport("kernel32", EntryPoint = "SetDllDirectory", CharSet = CharSet.Unicode, SetLastError = true)]
         private static extern bool SetDllDirectoryNative(string lpPathName);
 
