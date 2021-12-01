74538a0f00442cd98aec6f913c0d141668adbb27
gsemac <gabe.semac@gmail.com>
2021-11-30 17:22:54 -0700

    Updated default user agent

--- src/Gsemac.Net/Properties/DefaultHeaders.Designer.cs
+++ src/Gsemac.Net/Properties/DefaultHeaders.Designer.cs
@@ -79,7 +79,7 @@ namespace Gsemac.Net.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36.
+        ///   Looks up a localized string similar to Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36.
         /// </summary>
         internal static string UserAgent {
             get {
