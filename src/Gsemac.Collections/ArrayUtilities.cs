a92a8082389d22e5904fd892b4177fc4e8af754e
gsemac <gabe.semac@gmail.com>
2021-12-05 22:08:15 -0700

    Added ArrayUtilities

---
+++ src/Gsemac.Collections/ArrayUtilities.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Gsemac.Collections {
+
+    public static class ArrayUtilities {
+
+        // Public members
+
+        public static void Shift<T>(T[] array, int offset) {
+
+            if (array is null)
+                throw new ArgumentNullException(nameof(array));
+
+            if (Math.Abs(offset) >= array.Length) {
+
+                Array.Clear(array, 0, array.Length);
+
+            }
+            else if (offset < 0) {
+
+                offset = Math.Abs(offset);
+
+                Array.Copy(array, offset, array, 0, offset + 1);
+                Array.Clear(array, offset + 1, array.Length - offset - 1);
+
+            }
+            else if (offset > 0) {
+
+                Array.Copy(array, 0, array, offset, offset + 1);
+                Array.Clear(array, 0, offset);
+
+            }
+
+        }
+
+    }
+
+}
---
+++ tests/Gsemac.Collections.Tests/ArrayUtilitiesTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gsemac.Collections.Tests {
+
+    [TestClass]
+    public class ArrayUtilitiesTests {
+
+        // Shift
+
+        [TestMethod]
+        public void TestShiftWithPositiveOffset() {
+
+            int[] beforeItems = new int[] { 1, 2, 3, 4, 5 };
+            int[] afterItems = new int[] { 0, 0, 1, 2, 3 };
+
+            ArrayUtilities.Shift(beforeItems, 2);
+
+            CollectionAssert.AreEqual(afterItems, beforeItems);
+
+        }
+        [TestMethod]
+        public void TestShiftWithNegativeOffset() {
+
+            int[] beforeItems = new int[] { 1, 2, 3, 4, 5 };
+            int[] afterItems = new int[] { 3, 4, 5, 0, 0 };
+
+            ArrayUtilities.Shift(beforeItems, -2);
+
+            CollectionAssert.AreEqual(afterItems, beforeItems);
+
+        }
+        [TestMethod]
+        public void TestShiftWithPositiveOffsetGreaterThanArrayLength() {
+
+            int[] beforeItems = new int[] { 1, 2, 3, 4, 5 };
+            int[] afterItems = new int[] { 0, 0, 0, 0, 0 };
+
+            ArrayUtilities.Shift(beforeItems, 100);
+
+            CollectionAssert.AreEqual(afterItems, beforeItems);
+
+        }
+        [TestMethod]
+        public void TestShiftWithNegativeOffsetGreaterThanArrayLength() {
+
+            int[] beforeItems = new int[] { 1, 2, 3, 4, 5 };
+            int[] afterItems = new int[] { 0, 0, 0, 0, 0 };
+
+            ArrayUtilities.Shift(beforeItems, -100);
+
+            CollectionAssert.AreEqual(afterItems, beforeItems);
+
+        }
+
+    }
+
+}
