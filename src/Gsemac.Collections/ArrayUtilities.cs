11ba0ee50431d277b57599a18081d8a49ec7738d
gsemac <gabe.semac@gmail.com>
2021-12-06 13:32:23 -0700

    Added Rotate method to ArrayUtilities

--- src/Gsemac.Collections/ArrayUtilities.cs
+++ src/Gsemac.Collections/ArrayUtilities.cs
@@ -6,6 +6,38 @@ namespace Gsemac.Collections {
 
         // Public members
 
+        public static void Rotate<T>(T[] array, int offset) {
+
+            if (array is null)
+                throw new ArgumentNullException(nameof(array));
+
+            offset %= array.Length;
+
+            if (offset == 0)
+                return;
+
+            T[] buffer = new T[Math.Abs(offset)];
+
+            if (offset < 0) {
+
+                Array.Copy(array, 0, buffer, 0, Math.Abs(offset));
+
+                Shift(array, offset);
+
+                Array.Copy(buffer, 0, array, array.Length - Math.Abs(offset), buffer.Length);
+
+            }
+            else if (offset > 0) {
+
+                Array.Copy(array, array.Length - offset, buffer, 0, offset);
+
+                Shift(array, offset);
+
+                Array.Copy(buffer, array, buffer.Length);
+
+            }
+
+        }
         public static void Shift<T>(T[] array, int offset) {
 
             if (array is null)
--- tests/Gsemac.Collections.Tests/ArrayUtilitiesTests.cs
+++ tests/Gsemac.Collections.Tests/ArrayUtilitiesTests.cs
@@ -5,6 +5,53 @@ namespace Gsemac.Collections.Tests {
     [TestClass]
     public class ArrayUtilitiesTests {
 
+        // Rotate
+
+        [TestMethod]
+        public void TestRotateWithPositiveOffset() {
+
+            int[] beforeItems = new int[] { 1, 2, 3, 4, 5 };
+            int[] afterItems = new int[] { 4, 5, 1, 2, 3 };
+
+            ArrayUtilities.Rotate(beforeItems, 2);
+
+            CollectionAssert.AreEqual(afterItems, beforeItems);
+
+        }
+        [TestMethod]
+        public void TestRotateWithNegativeOffset() {
+
+            int[] beforeItems = new int[] { 1, 2, 3, 4, 5 };
+            int[] afterItems = new int[] { 3, 4, 5, 1, 2 };
+
+            ArrayUtilities.Rotate(beforeItems, -2);
+
+            CollectionAssert.AreEqual(afterItems, beforeItems);
+
+        }
+        [TestMethod]
+        public void TestRotateWithPositiveOffsetGreaterThanArrayLength() {
+
+            int[] beforeItems = new int[] { 1, 2, 3, 4, 5 };
+            int[] afterItems = new int[] { 4, 5, 1, 2, 3 };
+
+            ArrayUtilities.Rotate(beforeItems, 7);
+
+            CollectionAssert.AreEqual(afterItems, beforeItems);
+
+        }
+        [TestMethod]
+        public void TestRotateWithNegativeOffsetGreaterThanArrayLength() {
+
+            int[] beforeItems = new int[] { 1, 2, 3, 4, 5 };
+            int[] afterItems = new int[] { 3, 4, 5, 1, 2 };
+
+            ArrayUtilities.Rotate(beforeItems, -7);
+
+            CollectionAssert.AreEqual(afterItems, beforeItems);
+
+        }
+
         // Shift
 
         [TestMethod]
