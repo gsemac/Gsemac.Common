83e9ddc4c5308d63d4c3312a5cb990d3b3a56006
gsemac <gabe.semac@gmail.com>
2021-12-07 19:27:30 -0700

    Removed GetKnownFileFormats method

--- src/Gsemac.IO/IFileFormatFactory.cs
+++ src/Gsemac.IO/IFileFormatFactory.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 
 namespace Gsemac.IO {
 
     public interface IFileFormatFactory {
-
-        IEnumerable<IFileFormat> GetKnownFileFormats();
 
         IFileFormat FromMimeType(IMimeType mimeType);
         IFileFormat FromFileExtension(string fileExtension);
