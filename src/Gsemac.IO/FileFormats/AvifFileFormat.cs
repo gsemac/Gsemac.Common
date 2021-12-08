c9ac0adf86f92b205d23e67259a2409d111b414e
gsemac <gabe.semac@gmail.com>
2021-12-07 20:02:30 -0700

    Added signature for AVIF files

--- src/Gsemac.IO/FileFormats/AvifFileFormat.cs
+++ src/Gsemac.IO/FileFormats/AvifFileFormat.cs
@@ -6,6 +6,9 @@ namespace Gsemac.IO.FileFormats {
         FileFormatBase {
 
         public override IEnumerable<string> Extensions => new[] { ".avif" };
+        public override IEnumerable<IFileSignature> Signatures => new[] {
+            new FileSignature(0x66, 0x74, 0x79, 0x70, 0x61, 0x76, 0x69, 0x66) { Offset = 4 },
+        };
         public override IMimeType MimeType => new MimeType("image/avif");
 
     }
