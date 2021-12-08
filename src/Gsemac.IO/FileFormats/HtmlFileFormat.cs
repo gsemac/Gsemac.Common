41918bb8289f53610101eeb7fff0f7ef175ea1f8
gsemac <gabe.semac@gmail.com>
2021-12-07 19:28:16 -0700

    Added HtmlFileFormat class

---
+++ src/Gsemac.IO/FileFormats/HtmlFileFormat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Gsemac.IO.FileFormats {
+
+    public sealed class HtmlFileFormat :
+        FileFormatBase {
+
+        public override IEnumerable<string> Extensions => new[] { ".html", ".htm" };
+        public override IEnumerable<IFileSignature> Signatures => new[] {
+            new TextFileSignature("<html ", FileSignatureOptions.CaseInsensitive),
+            new TextFileSignature("<!doctype html>", FileSignatureOptions.CaseInsensitive),
+        };
+        public override IMimeType MimeType => new MimeType("text/html");
+
+    }
+
+}
---
+++ src/Gsemac.IO/FileFormats/TextFormat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Gsemac.IO.FileFormats {
+
+    public static class TextFormat {
+
+        public static IFileFormat Html => new HtmlFileFormat();
+
+        public static IEnumerable<IFileFormat> GetFormats() {
+
+            return typeof(TextFormat).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(property => property.GetValue(null, null))
+                .Cast<IFileFormat>();
+
+        }
+
+    }
+
+}
