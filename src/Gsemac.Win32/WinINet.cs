8b776e90500752de49f002d6f65c05b6b04dc66b
gsemac <gabe.semac@gmail.com>
2021-12-27 17:06:57 -0700

    Changed data type of dwFlags from int to uint The paramter type is DWORD, which is better represented as a 32-bit unsigned integer.

--- src/Gsemac.Win32/WinINet.cs
+++ src/Gsemac.Win32/WinINet.cs
@@ -7,12 +7,28 @@ namespace Gsemac.Win32 {
 
         // Public members
 
+        /// <summary>
+        /// Creates a cookie associated with the specified URL.
+        /// </summary>
+        /// <param name="lpszUrlName">Pointer to a null-terminated string that specifies the URL for which the cookie should be set.</param>
+        /// <param name="lpszCookieName">Pointer to a null-terminated string that specifies the name to be associated with the cookie data. If this parameter is <see cref="null"/>, no name is associated with the cookie.</param>
+        /// <param name="lpszCookieData">Pointer to the actual data to be associated with the URL.</param>
+        /// <returns>Returns <see cref="true"/> if successful, or <see cref="false"/> otherwise. To get a specific error message, call <see cref="Marshal.GetLastWin32Error"/>.</returns>
         public static bool InternetSetCookie(string lpszUrlName, string lpszCookieName, string lpszCookieData) {
 
             return InternetSetCookieNative(lpszUrlName, lpszCookieName, lpszCookieData);
 
         }
-        public static bool InternetSetCookieEx(string lpszUrlName, string lpszCookieName, string lpszCookieData, int dwFlags, IntPtr dwReserved) {
+        /// <summary>
+        /// The InternetSetCookieEx function creates a cookie with a specified name that is associated with a specified URL. This function differs from the <see cref="InternetSetCookie"/> function by being able to create third-party cookies.
+        /// </summary>
+        /// <param name="lpszUrlName">Pointer to a null-terminated string that specifies the URL for which the cookie should be set.</param>
+        /// <param name="lpszCookieName">Pointer to a null-terminated string that specifies the name to be associated with the cookie data. If this parameter is <see cref="null"/>, no name is associated with the cookie.</param>
+        /// <param name="lpszCookieData">Pointer to a null-terminated string that contains the data to be associated with the new cookie.<para></para>If this pointer is <see cref="null"/>, <see cref="InternetSetCookieEx"/> fails with an ERROR_INVALID_PARAMETER error.</param>
+        /// <param name="dwFlags">Flags that control how the function retrieves cookie data.</param>
+        /// <param name="dwReserved"><see cref="null"/>, or contains a pointer to a Platform-for-Privacy-Protection (P3P) header to be associated with the cookie.</param>
+        /// <returns>Returns a member of the InternetCookieState enumeration if successful, or <see cref="false"/> if the function fails. On failure, if a call to GetLastError returns ERROR_NOT_ENOUGH_MEMORY, insufficient system memory was available.</returns>
+        public static bool InternetSetCookieEx(string lpszUrlName, string lpszCookieName, string lpszCookieData, uint dwFlags, IntPtr dwReserved) {
 
             return InternetSetCookieExNative(lpszUrlName, lpszCookieName, lpszCookieData, dwFlags, dwReserved);
 
@@ -23,7 +39,7 @@ namespace Gsemac.Win32 {
         [DllImport("wininet", EntryPoint = "InternetSetCookie", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern bool InternetSetCookieNative(string lpszUrlName, string lpszCookieName, string lpszCookieData);
         [DllImport("wininet", EntryPoint = "InternetSetCookieEx", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern bool InternetSetCookieExNative(string lpszUrlName, string lpszCookieName, string lpszCookieData, int dwFlags, IntPtr dwReserved);
+        private static extern bool InternetSetCookieExNative(string lpszUrlName, string lpszCookieName, string lpszCookieData, uint dwFlags, IntPtr dwReserved);
 
     }
 
