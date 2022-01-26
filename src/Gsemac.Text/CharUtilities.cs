d7873153fce2b8e519f1a3ec4f4d15fa42fcdaec
gsemac <gabe.semac@gmail.com>
2022-01-25 20:56:46 -0700

    Added additional enclosing punctuation

--- src/Gsemac.Text/CharUtilities.cs
+++ src/Gsemac.Text/CharUtilities.cs
@@ -9,6 +9,22 @@ namespace Gsemac.Text {
 
         // Public members
 
+        public static bool IsLeftEnclosingPunctuation(char value) {
+
+            return enclosingPunctuationDict.Value.ContainsKey(value);
+
+        }
+        public static bool IsRightEnclosingPunctuation(char value) {
+
+            return enclosingPunctuationDict.Value.Values.Contains(value);
+
+        }
+        public static bool IsEnclosingPunctuation(char value) {
+
+            return IsLeftEnclosingPunctuation(value) ||
+                IsRightEnclosingPunctuation(value);
+
+        }
         public static char GetOppositeEnclosingPunctuation(char value) {
 
             if (enclosingPunctuationDict.Value.TryGetValue(value, out char complementChar))
@@ -20,25 +36,10 @@ namespace Gsemac.Text {
             return value;
 
         }
-        public static bool IsEnclosingPunctuation(char value) {
 
-            return IsLeftEnclosingPunctuation(value) ||
-                IsRightEnclosingPunctuation(value);
-
-        }
-        public static bool IsLeftEnclosingPunctuation(char value) {
-
-            return enclosingPunctuationDict.Value.ContainsKey(value);
-
-        }
         public static bool IsNewLine(char value) {
 
             return value == '\r' || value == '\n';
-
-        }
-        public static bool IsRightEnclosingPunctuation(char value) {
-
-            return enclosingPunctuationDict.Value.Values.Contains(value);
 
         }
         public static bool IsTerminalPunctuation(char value) {
@@ -89,54 +90,58 @@ namespace Gsemac.Text {
 
         private static IDictionary<char, char> CreateEnclosingPunctuationDict() {
 
+            // The following types of enclosing punctuation were found here: https://en.wikipedia.org/wiki/Bracket#Unicode
+
             return new Dictionary<char, char> {
-                // brackets
+                // General purpose
                 {'(', ')' },
                 {'[', ']' },
-                {'{', '}' },
+                // Technical/mathematical(common)
                 {'<', '>' },
-                {'❨', '❩' },
-                {'❪', '❫' },
-                {'❬', '❭' },
-                {'❰', '❱' },
-                {'❮', '❯' },
-                {'❲', '❳' },
-                {'❴', '❵' },
-                {'（', '）' },
-                {'［', '］' },
-                {'＜', '＞' },
-                {'｛', '｝' },
-                {'｟', '｠' },
-                // quotes
+                {'{', '}' },
+                // Quotation (Western texts)
                 {'\'', '\'' },
                 {'"', '"' },
-                {'‹', '›' },
                 {'«', '»' },
+                {'‹', '›' },
                 {'“', '”' },
                 {'‘', '’' },
-                {'⸢', '⸣' },
-                {'⸤', '⸥' },
-                {'〔', '〕' },
-                {'〖', '〗' },
-                {'〘', '〙' },
-                {'〚', '〛' },
-                {'〝', '〞' },
-                {'｢', '｣' },
-                {'〈', '〉' },
-                {'《', '》' },
-                {'「', '」' },
-                {'『', '』' },
-                {'【', '】' },
-                // math
+                {'‚', '‘' },
+                {'„', '“' },
+                // Floor and ceiling functions
                 { '⌈', '⌉' },
                 { '⌊', '⌋' },
+                // Quine corners
+                { '⌜', '⌝' },
+                { '⌞', '⌟' },
+                // Technical/mathematical (specialized)
                 { '⁽', '⁾' },
                 { '₍', '₎' },
+                { '⎛', '⎞' },
+                { '⎜', '⎟' },
+                { '⎝', '⎠' },
+                { '⎡', '⎤' },
+                { '⎢', '⎥' },
+                { '⎣', '⎦' },
+                { '⎧', '⎫' },
+                { '⎨', '⎬' },
+                { '⎩', '⎭' },
+                { '⎪', '⎪' },
+                { '⎰', '⎱' },
+                { '⎱', '⎰' },
+                { '⎴', '⎵' },
+                { '⎸', '⎹' },
+                { '⏜', '⏝' },
+                { '⏞', '⏟' },
+                { '⏠', '⏡' },
+                { '⟅', '⟆' },
+                { '⟓', '⟔' },
+                {'⟦', '⟧' },
                 {'⟨', '⟩' },
                 {'⟪', '⟫' },
-                {'⟦', '⟧' },
                 {'⟬', '⟭' },
                 {'⟮', '⟯' },
+                {'⦃', '⦄' },
                 {'⦅', '⦆' },
                 {'⦇', '⦈' },
                 {'⦉', '⦊' },
@@ -150,14 +155,56 @@ namespace Gsemac.Text {
                 {'⧘', '⧙' },
                 {'⧚', '⧛' },
                 {'⧼', '⧽' },
-                // other
+                // Half brackets
+                {'⸢', '⸣' },
+                {'⸤', '⸥' },
+                // Dingbats
+                { '❨', '❩' },
+                {'❪', '❫' },
+                {'❬', '❭' },
+                {'❰', '❱' },
+                {'❮', '❯' },
+                {'❲', '❳' },
+                {'❴', '❵' },
+                // Arabic (Quranic quotations)
+                {'﴾', '﴿' },
+                // N'Ko
+                {'⸝', '⸜' },
+                // Ogham
+                {'᚛', '᚜' },
+                // Tibetan
+                {'༺', '༻' },
+                {'༼', '༽' },
+                // New Testament editorial marks
                 { '⸂', '⸃' },
                 { '⸄', '⸅' },
                 { '⸉', '⸊' },
                 { '⸌', '⸍' },
+                // Medieval studies
                 { '⁅', '⁆' },
                 { '⸦', '⸧' },
                 { '⸨', '⸩' },
+                // Quotation (East-Asian texts)
+                {'〔', '〕' },
+                {'〖', '〗' },
+                {'〘', '〙' },
+                {'〚', '〛' },
+                {'〝', '〞' },
+                // Quotation (halfwidth East-Asian texts)
+                {'〈', '〉' },
+                {'｢', '｣' },
+                // Quotation (fullwidth East-Asian texts)
+                {'《', '》' },
+                {'「', '」' },
+                {'『', '』' },
+                {'【', '】' },
+                // General purpose (fullwidth East-Asian)
+                { '（', '）' },
+                {'［', '］' },
+                {'＜', '＞' },
+                // Technical/mathematical (fullwidth East-Asian)
+                {'｛', '｝' },
+                {'｟', '｠' },
             };
 
         }
