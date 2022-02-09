43b2965fa6a66f85a3be4a63935d23127ac0fbe5
gsemac <gabe.semac@gmail.com>
2022-02-08 23:13:38 -0700

    Added sample SVG file

---
+++ tests/Gsemac.IO.Tests/FileFormatFactoryTests.cs
@@ -0,0 +1,51 @@
+﻿using Gsemac.IO.Extensions;
+using Gsemac.IO.FileFormats;
+using Gsemac.IO.Tests.Properties;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Gsemac.IO.Tests {
+
+    [TestClass]
+    public abstract class FileFormatFactoryFromFileTestsBase {
+
+        // Public members
+
+        [TestMethod]
+        public void TestFileFormatFromFileReturnsExpectedFileFormat() {
+
+            IFileFormatFactory factory = new FileFormatFactory(new[] {
+                fileFormat,
+            });
+
+            Assert.AreEqual(fileFormat, factory.FromFile(Path.Combine(SamplePaths.ImagesSamplesDirectoryPath, testFileName)));
+
+        }
+
+        // Protected members
+
+        protected FileFormatFactoryFromFileTestsBase(IFileFormat fileFormat, string testFileName) {
+
+            this.fileFormat = fileFormat;
+            this.testFileName = testFileName;
+
+        }
+
+        // Private members
+
+        private readonly IFileFormat fileFormat;
+        private readonly string testFileName;
+
+    }
+
+    [TestClass]
+    public class SvgFileFormatFactoryFromFileTests :
+        FileFormatFactoryFromFileTestsBase {
+
+        public SvgFileFormatFactoryFromFileTests() :
+            base(ImageFormat.Svg, "test.svg") {
+        }
+
+    }
+
+}
