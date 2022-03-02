8010b6cda534a32d5c9b2223b1428100f7667cf1
gsemac <gabe.semac@gmail.com>
2022-03-01 23:45:11 -0500

    Added ImageBase class

---
+++ src/Gsemac.Drawing/Imaging/ImageBase.cs
@@ -0,0 +1,42 @@
+﻿using Gsemac.IO;
+using System;
+using System.Drawing;
+
+namespace Gsemac.Drawing.Imaging {
+
+    public abstract class ImageBase :
+        IImage {
+
+        // Public members
+
+        public virtual IAnimationInfo Animation => AnimationInfo.None;
+
+        public abstract int Width { get; }
+        public abstract int Height { get; }
+        public abstract IFileFormat Format { get; }
+        public abstract IImageCodec Codec { get; }
+
+        public Size Size => new Size(Width, Height);
+
+        public abstract IImage Clone();
+
+#if NETFRAMEWORK
+        public abstract Bitmap ToBitmap();
+#endif
+
+        public void Dispose() {
+
+            Dispose(disposing: true);
+
+            GC.SuppressFinalize(this);
+
+        }
+
+        // Protected members
+
+        protected virtual void Dispose(bool disposing) {
+        }
+
+    }
+
+}
