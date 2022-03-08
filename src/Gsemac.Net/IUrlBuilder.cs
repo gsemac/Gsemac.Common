4dadb5ef4ab779f4e1e527d68425955abbd6cb8b
gsemac <gabe.semac@gmail.com>
2022-03-08 14:35:13 -0700

    Added UrlBuilder class

---
+++ src/Gsemac.Net/IUrlBuilder.cs
@@ -0,0 +1,18 @@
+﻿namespace Gsemac.Net {
+
+    public interface IUrlBuilder {
+
+        IUrlBuilder WithScheme(string scheme);
+        IUrlBuilder WithUserName(string username);
+        IUrlBuilder WithPassword(string password);
+        IUrlBuilder WithHostname(string hostname);
+        IUrlBuilder WithPort(int port);
+        IUrlBuilder WithPath(string path);
+        IUrlBuilder WithFragment(string fragment);
+        IUrlBuilder WithQueryParameter(string name, string value);
+
+        IUrl Build();
+
+    }
+
+}
---
+++ src/Gsemac.Net/UrlBuilder.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Gsemac.Net {
+
+    public class UrlBuilder :
+        IUrlBuilder {
+
+        // Public members
+
+        public UrlBuilder() {
+
+            url = new Url();
+
+        }
+        public UrlBuilder(string url) {
+
+            this.url = new Url(url);
+
+        }
+        public UrlBuilder(IUrl url) {
+
+            if (url is null)
+                throw new ArgumentNullException(nameof(url));
+
+            this.url = new Url(url);
+
+        }
+
+        public IUrlBuilder WithFragment(string fragment) {
+
+            url.Fragment = fragment;
+
+            return this;
+
+        }
+        public IUrlBuilder WithHostname(string hostname) {
+
+            url.Hostname = hostname;
+
+            return this;
+
+        }
+        public IUrlBuilder WithPassword(string password) {
+
+            url.Password = password;
+
+            return this;
+
+        }
+        public IUrlBuilder WithPath(string path) {
+
+            url.Path = path;
+
+            return this;
+
+        }
+        public IUrlBuilder WithPort(int port) {
+
+            url.Port = port;
+
+            return this;
+
+        }
+        public IUrlBuilder WithQueryParameter(string name, string value) {
+
+            url.QueryParameters[name] = value;
+
+            return this;
+
+        }
+        public IUrlBuilder WithScheme(string scheme) {
+
+            url.Scheme = scheme;
+
+            return this;
+
+        }
+        public IUrlBuilder WithUserName(string userName) {
+
+            url.UserName = userName;
+
+            return this;
+
+        }
+
+        public IUrl Build() {
+
+            return url;
+
+        }
+
+        public override string ToString() {
+
+            return url.ToString();
+
+        }
+
+        // Private members
+
+        private readonly Url url;
+
+    }
+
+}
