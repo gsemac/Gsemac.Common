66d77730538bf81ab2ee62e0b62d814716175355
gsemac <gabe.semac@gmail.com>
2022-03-08 20:32:38 -0700

    Added some constants from winerror.h

---
+++ src/Gsemac.Win32/WinError.cs
@@ -0,0 +1,16 @@
+﻿namespace Gsemac.Win32 {
+
+    public static class WinError {
+
+        /// <summary>
+        /// The disk is full.
+        /// </summary>
+        public const int ERROR_HANDLE_DISK_FULL = 0x27;
+        /// <summary>
+        /// There is not enough space on the disk.
+        /// </summary>
+        public const int ERROR_DISK_FULL = 0x70;
+
+    }
+
+}
