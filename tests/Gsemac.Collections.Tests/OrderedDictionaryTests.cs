b30f881feb33483032c2f59c744c687457f6841f
gsemac <gabe.semac@gmail.com>
2022-04-11 15:31:19 -0600

    Added unit tests for OrderedDictionary

---
+++ tests/Gsemac.Collections.Tests/OrderedDictionaryTests.cs
@@ -0,0 +1,37 @@
+﻿using Gsemac.Collections.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Gsemac.Collections.Tests {
+
+    [TestClass]
+    public class OrderedDictionaryTests {
+
+        // Public members
+
+        [TestMethod]
+        public void TestKeysOrderingRespectsInsertionOrder() {
+
+            IDictionary<int, int> dict = new OrderedDictionary<int, int>(1);
+
+            for (int i = 0; i < 100; ++i)
+                dict.Add(i, i);
+
+            Assert.IsTrue(dict.Keys.IsSorted());
+
+        }
+        [TestMethod]
+        public void TestValuesOrderingRespectsInsertionOrder() {
+
+            IDictionary<int, int> dict = new OrderedDictionary<int, int>(1);
+
+            for (int i = 0; i < 100; ++i)
+                dict.Add(i, i);
+
+            Assert.IsTrue(dict.Values.IsSorted());
+
+        }
+
+    }
+
+}
