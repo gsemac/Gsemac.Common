1e1436626df06ba9b925ec5622edc8fd6553b931
gsemac <gabe.semac@gmail.com>
2022-05-31 19:23:17 -0600

    Added ToUri extension method for IUrlBuilder

---
+++ src/Gsemac.Net/Extensions/UrlBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Gsemac.Net.Extensions {
+
+    public static class UrlBuilderExtensions {
+
+        // Public members
+
+        public static Uri ToUri(this IUrlBuilder urlBuilder) {
+
+            if (urlBuilder is null)
+                throw new ArgumentNullException(nameof(urlBuilder));
+
+            return new Uri(urlBuilder.ToString());
+
+        }
+
+    }
+
+}
