bde6e888177a0d49e14c99f6a71b42ba17aca0bb
gsemac <gabe.semac@gmail.com>
2022-06-06 19:31:22 -0600

    Added CookieContainerExtensions class Added GetAllCookies extension method

---
+++ src/Gsemac.Polyfills/System/Net/CookieContainerExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Net;
+using System.Reflection;
+
+namespace Gsemac.Polyfills.System.Net {
+
+    public static class CookieContainerExtensions {
+
+        public static CookieCollection GetAllCookies(this CookieContainer cookieContainer) {
+            // Adapted from https://stackoverflow.com/a/50548676/5383169 (JJS)
+
+            if (cookieContainer is null)
+                throw new ArgumentNullException(nameof(cookieContainer));
+
+            CookieCollection cookies = new CookieCollection();
+
+            // We're not guaranteed that the implementation uses the following private members.
+            // In the event we can't find a particular member of the class, we'll just return an empty CookieCollection.
+
+            FieldInfo domainTableFieldInfo = typeof(CookieContainer)
+                .GetField("m_domainTable", BindingFlags.NonPublic | BindingFlags.GetField | BindingFlags.Instance);
+
+            if (domainTableFieldInfo is null || !(domainTableFieldInfo.GetValue(cookieContainer) is Hashtable domainTable))
+                return cookies;
+
+            foreach (string key in domainTable.Keys) {
+
+                var item = domainTable[key]; // System.Net.PathList
+
+                PropertyInfo valuesPropertyInfo = item.GetType().GetProperty("Values");
+
+                if (valuesPropertyInfo is object && valuesPropertyInfo.GetGetMethod().Invoke(item, null) is ICollection items) {
+
+                    foreach (CookieCollection cookieCollection in items) {
+
+                        foreach (Cookie cookie in cookieCollection)
+                            cookies.Add(cookie);
+
+                    }
+
+                }
+
+            }
+
+            return cookies;
+
+        } // .NET 6 and later
+
+    }
+
+}
