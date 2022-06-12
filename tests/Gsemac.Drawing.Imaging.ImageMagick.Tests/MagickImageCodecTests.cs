cf5fca958c3fac2342ade03fa8a26911adb02a26
gsemac <gabe.semac@gmail.com>
2022-06-11 22:26:49 -0600

    Added additional unit tests for MagickImageCodec

--- tests/Gsemac.Drawing.Imaging.ImageMagick.Tests/MagickImageCodecTests.cs
+++ tests/Gsemac.Drawing.Imaging.ImageMagick.Tests/MagickImageCodecTests.cs
@@ -1,6 +1,8 @@
+using Gsemac.Drawing.Extensions;
 using Gsemac.Drawing.Imaging.Extensions;
 using Gsemac.Drawing.Imaging.ImageMagick.Tests.Properties;
 using Gsemac.IO;
+using Gsemac.IO.FileFormats;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.IO;
@@ -16,6 +18,42 @@ namespace Gsemac.Drawing.Imaging.ImageMagick.Tests {
         public void TestAnimationInfoIsValidWithAnimatedGif() {
 
             using (IImage image = ImageFromFile("animated.gif")) {
+
+                Assert.IsNotNull(image);
+
+                Assert.AreEqual(TimeSpan.FromMilliseconds(200), image.AnimationDelay);
+                Assert.AreEqual(3, image.FrameCount);
+                Assert.AreEqual(0, image.AnimationIterations);
+
+            }
+
+        }
+        [TestMethod]
+        public void TestAnimationInfoIsValidWithAnimatedGifAndStaticDecoderMode() {
+
+            IImageDecoderOptions options = new ImageDecoderOptions() {
+                Mode = ImageDecoderMode.Static,
+            };
+
+            using (IImage image = ImageFromFile("animated.gif", options)) {
+
+                Assert.IsNotNull(image);
+
+                Assert.AreEqual(TimeSpan.FromMilliseconds(200), image.AnimationDelay);
+                Assert.AreEqual(3, image.FrameCount);
+                Assert.AreEqual(0, image.AnimationIterations);
+
+            }
+
+        }
+        [TestMethod]
+        public void TestAnimationInfoIsValidWithAnimatedGifAndMetadataDecoderMode() {
+
+            IImageDecoderOptions options = new ImageDecoderOptions() {
+                Mode = ImageDecoderMode.Metadata,
+            };
+
+            using (IImage image = ImageFromFile("animated.gif", options)) {
 
                 Assert.IsNotNull(image);
 
@@ -58,17 +96,56 @@ namespace Gsemac.Drawing.Imaging.ImageMagick.Tests {
 
         }
 
+        [TestMethod]
+        public void TestAnimationInfoIsValidAfterConvertingAnimatedWebPToAnimatedGif() {
+
+            // Animation info should remain the same after converting an animated image to another format.
+
+            using (Stream animatedGifStream = new MemoryStream()) {
+
+                using (IImage image = ImageFromFile("animated.webp"))
+                    image.Save(animatedGifStream, ImageFormat.Gif);
+
+                animatedGifStream.Seek(0, SeekOrigin.Begin);
+
+                using (IImage image = ImageFromStream(animatedGifStream)) {
+
+                    Assert.IsNotNull(image);
+
+                    Assert.AreEqual(TimeSpan.FromMilliseconds(200), image.AnimationDelay);
+                    Assert.AreEqual(3, image.FrameCount);
+                    Assert.AreEqual(0, image.AnimationIterations);
+
+                }
+
+            }
+
+        }
+
         // Private members
 
+        private static IImage ImageFromStream(Stream stream) {
+
+            IImageCodec codec = new MagickImageCodec();
+
+            return codec.Decode(stream);
+
+        }
         private static IImage ImageFromFile(string fileName) {
+
+            return ImageFromFile(fileName, ImageDecoderOptions.Default);
+
+        }
+        private static IImage ImageFromFile(string fileName, IImageDecoderOptions options) {
 
             IFileFormat fileFormat = FileFormatFactory.Default.FromFileExtension(fileName);
             IImageCodec codec = new MagickImageCodec(fileFormat);
 
             using (Stream stream = File.OpenRead(Path.Combine(SamplePaths.ImagesSamplesDirectoryPath, fileName)))
-                return codec.Decode(stream);
+                return codec.Decode(stream, options);
 
         }
+
 
     }
 
