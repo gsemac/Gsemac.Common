d01f9fb437e42e911b33d0dc78df68116a6f6ac4
gsemac <gabe.semac@gmail.com>
2022-06-11 23:51:45 -0600

    Added DictionaryExtensions class Added `Reverse` method for creating a reverse-lookup dictionary.

---
+++ src/Gsemac.Collections/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gsemac.Collections.Extensions {
+
+    public static class DictionaryExtensions {
+
+        // Public members
+
+        public static IDictionary<TValue, TKey> Reverse<TKey, TValue>(this IDictionary<TKey, TValue> dictionary) {
+
+            if (dictionary is null)
+                throw new ArgumentNullException(nameof(dictionary));
+
+            IDictionary<TValue, TKey> result = new Dictionary<TValue, TKey>();
+
+            // If the dictionary contains duplicate keys (values), only the latest value will be mapped to the key.
+
+            foreach (var pair in dictionary)
+                result[pair.Value] = pair.Key;
+
+            return result;
+
+        }
+
+    }
+
+}
