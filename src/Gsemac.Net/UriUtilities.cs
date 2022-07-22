1654ff38b5f6f5904c53ddc3a835b16d18b7c1f4
gsemac <gabe.semac@gmail.com>
2022-07-22 15:54:43 -0600

    Added SetPathCanonicalizationEnabled method to UriUtilities

--- src/Gsemac.Net/UriUtilities.cs
+++ src/Gsemac.Net/UriUtilities.cs
@@ -7,47 +7,74 @@ namespace Gsemac.Net {
 
         // Public members
 
-        public static void SetCanonicalizeAsFilePath(bool enabled) {
+        public static bool SetFilePathCanonicalizationEnabled(bool enabled) {
 
-            // .NET 4.0's Uri class implementation doesn't work correctly for URIs that contain trailing periods (fixed in .NET 4.5).
+            // .NET 4.0's Uri implementation doesn't work correctly for URIs that contain trailing periods (fixed in .NET 4.5).
             // https://stackoverflow.com/questions/856885/httpwebrequest-to-url-with-dot-at-the-end
             // The solution below is the one given in the official bug report, as given here: https://stackoverflow.com/a/2285321 (Jon Davis)
 
             MethodInfo getSyntax = typeof(UriParser).GetMethod("GetSyntax", BindingFlags.Static | BindingFlags.NonPublic);
             FieldInfo flagsField = typeof(UriParser).GetField("m_Flags", BindingFlags.Instance | BindingFlags.NonPublic);
 
-            if (getSyntax is object && flagsField is object) {
+            if (getSyntax is null || flagsField is null)
+                return false;
 
-                foreach (string scheme in new[] { "http", "https" }) {
+            foreach (string scheme in new[] { "http", "https" }) {
 
-                    UriParser parser = (UriParser)getSyntax.Invoke(null, new object[] { scheme });
+                UriParser parser = (UriParser)getSyntax.Invoke(null, new object[] { scheme });
 
-                    if (parser is object) {
+                if (parser is null)
+                    continue;
 
-                        UriSyntaxFlags flagsValue = (UriSyntaxFlags)flagsField.GetValue(parser);
+                UriSyntaxFlags flags = (UriSyntaxFlags)flagsField.GetValue(parser);
 
-                        if (enabled) {
+                if (enabled)
+                    flags |= UriSyntaxFlags.CanonicalizeAsFilePath;
+                else
+                    flags &= ~UriSyntaxFlags.CanonicalizeAsFilePath;
 
-                            // Set the CanonicalizeAsFilePath flag
-
-                            if (!flagsValue.HasFlag(UriSyntaxFlags.CanonicalizeAsFilePath))
-                                flagsField.SetValue(parser, flagsValue | UriSyntaxFlags.CanonicalizeAsFilePath);
-
-                        }
-                        else {
-
-                            // Clear the CanonicalizeAsFilePath flag
-
-                            if (flagsValue.HasFlag(UriSyntaxFlags.CanonicalizeAsFilePath))
-                                flagsField.SetValue(parser, flagsValue & ~UriSyntaxFlags.CanonicalizeAsFilePath);
-
-                        }
-
-                    }
-
-                }
+                flagsField.SetValue(parser, flags);
 
             }
+
+            return true;
+
+        }
+
+        public static bool SetPathCanonicalizationEnabled(Uri uri, bool enabled) {
+
+            // .NET 4.0's Uri implementation doesn't work correctly for URIs that contain escaped slashes (fixed in .NET 4.5).
+            // It attempts to unescape escaped slashes, which can cause problems for endpoints that expect them to be escaped:
+            // https://docs.microsoft.com/en-us/dotnet/api/system.uri?view=netcore-3.1#remarks
+
+            // See the following discussions of the issue:
+            // https://stackoverflow.com/q/59846369/5383169
+            // https://stackoverflow.com/q/781205/5383169
+
+            // The following solution is adapted from the one given here: https://stackoverflow.com/a/784937/5383169 (Rasmus Faber)
+
+            if (uri is null)
+                throw new ArgumentNullException(nameof(uri));
+
+            // We need to access the "PathAndQuery" property before setting the flag.
+
+            string _ = uri.PathAndQuery;
+
+            FieldInfo flagsField = typeof(Uri).GetField("m_Flags", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            if (flagsField is null)
+                return false;
+
+            UriFlags flags = (UriFlags)(ulong)flagsField.GetValue(uri);
+
+            if (enabled)
+                flags |= UriFlags.PathNotCanonical | UriFlags.QueryNotCanonical;
+            else
+                flags &= ~(UriFlags.PathNotCanonical | UriFlags.QueryNotCanonical);
+
+            flagsField.SetValue(uri, (ulong)flags);
+
+            return true;
 
         }
 
@@ -56,6 +83,14 @@ namespace Gsemac.Net {
         [Flags]
         private enum UriSyntaxFlags {
             CanonicalizeAsFilePath = 0x1000000,
+        }
+
+        // https://referencesource.microsoft.com/#System/net/System/URI.cs,a5f3168ce0003968,references
+
+        [Flags]
+        private enum UriFlags {
+            PathNotCanonical = 0x10,
+            QueryNotCanonical = 0x20,
         }
 
     }
---
+++ tests/Gsemac.Net.Tests/UriUtilitiesTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Gsemac.Net.Tests {
+
+    [TestClass]
+    public class UriUtilitiesTests {
+
+        // SetFilePathCanonicalizationEnabled
+
+        //[TestMethod]
+        //public void TestAbsoluteUriWithFilePathCanonicalizationEnabled() {
+
+        //    UriUtilities.SetFilePathCanonicalizationEnabled(true);
+
+        //    Uri uri = new("https://example.com/file.");
+
+        //    Assert.AreEqual("https://example.com/file", uri.AbsoluteUri);
+
+        //}
+        [TestMethod]
+        public void TestAbsoluteUriWithFilePathCanonicalizationDisabled() {
+
+            UriUtilities.SetFilePathCanonicalizationEnabled(false);
+
+            Uri uri = new("https://example.com/file.");
+
+            Assert.AreEqual("https://example.com/file.", uri.AbsoluteUri);
+
+        }
+
+        // SetPathCanonicalizationEnabled
+
+        //[TestMethod]
+        //public void TestAbsoluteUriWithPathCanonicalizationEnabled() {
+
+        //    Uri uri = new("https://example.com/%5Cfile");
+
+        //    UriUtilities.SetPathCanonicalizationEnabled(uri, true);
+
+        //    Assert.AreEqual("https://example.com//file", uri.AbsoluteUri);
+
+        //}
+        [TestMethod]
+        public void TestAbsoluteUriWithPathCanonicalizationDisabled() {
+
+            Uri uri = new("https://example.com/%5Cfile");
+
+            UriUtilities.SetPathCanonicalizationEnabled(uri, false);
+
+            Assert.AreEqual("https://example.com/%5Cfile", uri.AbsoluteUri);
+
+        }
+
+    }
+
+}
