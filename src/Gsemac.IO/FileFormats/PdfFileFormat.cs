d848586e326e5d533aea29c8b12b0675df19c114
gsemac <gabe.semac@gmail.com>
2022-09-01 22:12:09 -0600

    Added PdfFileFormat class

---
+++ src/Gsemac.IO/FileFormats/PdfFileFormat.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Gsemac.IO.FileFormats {
+
+    public sealed class PdfFileFormat :
+        FileFormatBase {
+
+        public override IEnumerable<string> Extensions => new[] { ".pdf" };
+        public override IEnumerable<IFileSignature> Signatures => new[] {
+            new FileSignature(0x25, 0x50, 0x44, 0x46, 0x2D),
+        };
+        public override IMimeType MimeType => new MimeType("application/pdf");
+
+    }
+
+}
