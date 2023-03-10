5343a18674c1fe79040700fb3039fadd6f9fe6fd
gsemac <gabe.semac@gmail.com>
2023-03-09 22:33:20 -0700

    Added ProcessUtilities class

---
+++ src/Gsemac.Core/ProcessUtilities.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Gsemac.Core {
+
+    public static class ProcessUtilities {
+
+        // Public members
+
+        public static IEnumerable<Process> GetProcessesByFileName(string fileName) {
+
+            if (string.IsNullOrWhiteSpace(fileName))
+                return Enumerable.Empty<Process>();
+
+            string processName = Path.GetFileNameWithoutExtension(fileName);
+
+            if (string.IsNullOrEmpty(processName))
+                return Enumerable.Empty<Process>();
+
+            return Process.GetProcessesByName(processName);
+
+        }
+        public static IEnumerable<Process> GetProcessesByFilePath(string filePath) {
+
+            return GetProcessesByFileName(filePath)
+                .Where(process => ProcessHasFilePath(process, filePath));
+
+        }
+
+        // Private members
+
+        private static bool ProcessHasFilePath(Process process, string filePath) {
+
+            if (process is null)
+                throw new ArgumentNullException(nameof(process));
+
+            if (string.IsNullOrWhiteSpace(filePath))
+                return false;
+
+            string processExecutablePath = process.MainModule.FileName;
+
+            return !string.IsNullOrWhiteSpace(processExecutablePath) && processExecutablePath.Equals(filePath);
+
+        }
+
+    }
+
+}
