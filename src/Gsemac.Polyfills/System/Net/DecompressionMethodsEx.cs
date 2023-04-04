46d3898f5b6515d708b333e1370241d064859701
gsemac <gabe.semac@gmail.com>
2023-04-04 13:28:24 -0600

    Added DecompressionMethodsEx enum

---
+++ src/Gsemac.Polyfills/System/Net/DecompressionMethodsEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+
+namespace Gsemac.Polyfills.System.Net {
+
+    /// <inheritdoc cref="DecompressionMethods"/>
+    [Flags]
+    public enum DecompressionMethodsEx {
+        /// <summary>
+        /// Use all compression-decompression algorithms.
+        /// </summary>
+        All = -1,
+        /// <inheritdoc cref="DecompressionMethods.None"/>
+        None = 0,
+        /// <inheritdoc cref="DecompressionMethods.GZip"/>
+        GZip = 1,
+        /// <inheritdoc cref="DecompressionMethods.Deflate"/>
+        Deflate = 2,
+        /// <summary>
+        /// Use the Brotli compression-decompression algorithm.
+        /// </summary>
+        Brotli = 4,
+    }
+
+}
