1137113bc5e3dc60be8c6b8a8a8c280139a439f8
gsemac <gabe.semac@gmail.com>
2023-04-09 18:49:47 -0600

    Added unit tests for JpegFileFormat class

---
+++ tests/Gsemac.IO.Tests/JpegFileFormatTests.cs
@@ -0,0 +1,22 @@
+﻿using Gsemac.IO.FileFormats;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Gsemac.IO.Tests {
+
+    [TestClass]
+    public class JpegFileFormatTests {
+
+        [TestMethod]
+        public void TestExtensionReturnsJpgExtension() {
+
+            // The default file extension for the JPEG format should be "jpg".
+            // Even though this is a shortened version of the "jpeg" extension, it's the one most commonly used.
+
+            Assert.AreEqual(".jpg", ImageFormat.Jpeg.Extensions.FirstOrDefault());
+
+        }
+
+    }
+
+}
