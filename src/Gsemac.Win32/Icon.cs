f25043ce6e8485d705ca1fc25a65608659b0e4ea
gsemac <gabe.semac@gmail.com>
2023-04-11 18:06:28 -0600

    Changed parameter name in FromFilePath method

--- src/Gsemac.Win32/Icon.cs
+++ src/Gsemac.Win32/Icon.cs
@@ -21,14 +21,14 @@ namespace Gsemac.Win32 {
             return FromFilePath(filePath, IconSize.Small);
 
         }
-        public static Icon FromFilePath(string fileExtension, IconSize iconSize) {
+        public static Icon FromFilePath(string filePath, IconSize iconSize) {
 
             SHFILEINFO shInfo = new SHFILEINFO {
                 szDisplayName = new string((char)0, MAX_PATH),
                 szTypeName = new string((char)0, 80)
             };
 
-            Shell32.SHGetFileInfo(fileExtension, FILE_ATTRIBUTE_NORMAL, ref shInfo, Marshal.SizeOf(shInfo), SHGFI_ICON | (int)iconSize | SHGFI_USEFILEATTRIBUTES);
+            Shell32.SHGetFileInfo(filePath, FILE_ATTRIBUTE_NORMAL, ref shInfo, Marshal.SizeOf(shInfo), SHGFI_ICON | (int)iconSize | SHGFI_USEFILEATTRIBUTES);
 
             return FromHandle(shInfo.hIcon);
 
