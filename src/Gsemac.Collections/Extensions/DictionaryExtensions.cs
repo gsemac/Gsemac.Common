5434ecf24ae791dcd887ccf668a8f704a7b49772
gsemac <gabe.semac@gmail.com>
2023-06-03 20:15:06 -0600

    Added DictionaryUtilities class

--- src/Gsemac.Collections/Extensions/DictionaryExtensions.cs
+++ src/Gsemac.Collections/Extensions/DictionaryExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Gsemac.Collections.Extensions {
 
@@ -9,17 +8,7 @@ namespace Gsemac.Collections.Extensions {
 
         public static IDictionary<TValue, TKey> Reverse<TKey, TValue>(this IDictionary<TKey, TValue> dictionary) {
 
-            if (dictionary is null)
-                throw new ArgumentNullException(nameof(dictionary));
-
-            IDictionary<TValue, TKey> result = new Dictionary<TValue, TKey>();
-
-            // If the dictionary contains duplicate keys (values), only the latest value will be mapped to the key.
-
-            foreach (var pair in dictionary)
-                result[pair.Value] = pair.Key;
-
-            return result;
+            return DictionaryUtilities.Reverse(dictionary);
 
         }
 
