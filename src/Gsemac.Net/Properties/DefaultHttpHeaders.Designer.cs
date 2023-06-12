0b96b59236e335bb80328b17dbd47ac9a2b28946
gsemac <gabe.semac@gmail.com>
2023-06-12 15:52:18 -0600

    Updated default user agent Updated the default user agent to the latest version of Google Chrome.

--- src/Gsemac.Net/Properties/DefaultHttpHeaders.Designer.cs
+++ src/Gsemac.Net/Properties/DefaultHttpHeaders.Designer.cs
@@ -88,7 +88,7 @@ namespace Gsemac.Net.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36.
+        ///   Looks up a localized string similar to Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36.
         /// </summary>
         internal static string UserAgent {
             get {
