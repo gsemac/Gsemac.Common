5310bec59be9042b17b8a6e9a8572eb8b9409f0f
gsemac <gabe.semac@gmail.com>
2023-06-19 18:51:56 -0600

    Fixed parameter types in `ControlExtensions`

--- src/Gsemac.Forms/Extensions/ControlExtensions.cs
+++ src/Gsemac.Forms/Extensions/ControlExtensions.cs
@@ -1,15 +1,36 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace Gsemac.Forms.Extensions {
 
     public static class ControlExtensions {
 
-        public static IControlState Save(this Control control, ControlStateOptions options = ControlStateOptions.Default) {
+        public static IControlState Save(this Control control) {
+
+            if (control is null)
+                throw new ArgumentNullException(nameof(control));
+
+            return ControlState.Save(control, ControlStateOptions.Default);
+
+        }
+        public static IControlState Save(this Control control, IControlStateOptions options) {
+
+            if (control is null)
+                throw new ArgumentNullException(nameof(control));
+
+            if (options is null)
+                throw new ArgumentNullException(nameof(options));
 
             return ControlState.Save(control, options);
 
         }
         public static void Restore(this Control control, IControlState state) {
+
+            if (control is null)
+                throw new ArgumentNullException(nameof(control));
+
+            if (state is null)
+                throw new ArgumentNullException(nameof(state));
 
             state.Restore(control);
 
