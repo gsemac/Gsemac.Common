9152a1b92791670a040aa61c0a21aa7f68cf5806
gsemac <gabe.semac@gmail.com>
2023-07-12 00:43:45 -0600

    Added `Chunk` extension method

---
+++ src/Gsemac.Polyfills/System/Linq/EnumerableExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gsemac.Polyfills.System.Linq {
+
+    public static class EnumerableExtensions {
+
+        // Public members
+
+        public static IEnumerable<TSource[]> Chunk<TSource>(this IEnumerable<TSource> source, int size) {
+
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+
+            if (size < 1)
+                throw new ArgumentOutOfRangeException(nameof(size));
+
+            // This could be improved by using an array buffer instead of a list, which works for all but the last chunk.
+
+            List<TSource> chunk = new List<TSource>(size);
+
+            foreach (TSource item in source) {
+
+                chunk.Add(item);
+
+                if (chunk.Count == size) {
+
+                    yield return chunk.ToArray();
+
+                    chunk = new List<TSource>(size);
+
+                }
+
+            }
+
+            if (chunk.Count > 0)
+                yield return chunk.ToArray();
+
+        } // .NET 6 and later
+
+    }
+
+}
---
+++ tests/Gsemac.Polyfills.Tests/System/Linq/EnumerableExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace Gsemac.Polyfills.System.Linq.Tests {
+
+    [TestClass]
+    public class EnumerableExtensionsTests {
+
+        // Chunk
+
+        [TestMethod]
+        public void TestChunkReturnsChunksOfCorrectLengths() {
+
+            int[] items = new[] { 1, 2, 3, 4, 5 };
+            int[][] chunkedItems = items.Chunk(2).ToArray();
+
+            Assert.AreEqual(3, chunkedItems.Length);
+
+            Assert.AreEqual(2, chunkedItems[0].Length);
+            Assert.AreEqual(2, chunkedItems[1].Length);
+            Assert.AreEqual(1, chunkedItems[2].Length);
+
+        }
+        [TestMethod]
+        public void TestChunkReturnsChunksOfCorrectItems() {
+
+            int[] items = new[] { 1, 2, 3, 4, 5 };
+            int[][] chunkedItems = items.Chunk(2).ToArray();
+
+            Assert.AreEqual(3, chunkedItems.Length);
+
+            CollectionAssert.AreEqual(new[] { 1, 2, }, chunkedItems[0]);
+            CollectionAssert.AreEqual(new[] { 3, 4, }, chunkedItems[1]);
+            CollectionAssert.AreEqual(new[] { 5, }, chunkedItems[2]);
+
+        }
+        [TestMethod]
+        public void TestChunkThrowsOnSizeLessThanOne() {
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new[] { 1, 2, 3, 4, 5 }.Chunk(0).ToArray());
+
+        }
+
+    }
+
+}
