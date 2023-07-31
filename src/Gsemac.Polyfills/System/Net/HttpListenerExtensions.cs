6b79886a4fe6a6ffeabd4461722da25e6076b02f
gsemac <gabe.semac@gmail.com>
2023-07-31 17:50:36 -0600

    Added `HttpListenerExtensions` class

---
+++ src/Gsemac.Polyfills/System/Net/HttpListenerExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Gsemac.Polyfills.System.Net {
+
+    public static class HttpListenerExtensions {
+
+        // Public members
+
+        /// <summary>
+        /// Waits for an incoming request as an asynchronous operation.
+        /// </summary>
+        public static Task<HttpListenerContext> GetContextAsync(this HttpListener httpListener) {
+
+            if (httpListener is null)
+                throw new ArgumentNullException(nameof(httpListener));
+
+            return Task<HttpListenerContext>.Factory.FromAsync(httpListener.BeginGetContext, httpListener.EndGetContext, null);
+
+        } // .NET Framework 4.5 and later
+
+    }
+
+}
