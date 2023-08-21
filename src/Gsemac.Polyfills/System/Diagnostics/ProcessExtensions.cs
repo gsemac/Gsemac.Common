a85f3d3c9971ac37c267860df5ccfe53c06509d6
gsemac <gabe.semac@gmail.com>
2023-08-21 17:07:43 -0600

    Added `ProcessExtensions` class

---
+++ src/Gsemac.Polyfills/System/Diagnostics/ProcessExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Gsemac.Polyfills.System.Diagnostics {
+
+    public static class ProcessExtensions {
+
+        // Public members
+
+        /// <summary>
+        /// Instructs the Process component to wait the specified amount of time for the associated process to exit.
+        /// </summary>
+        /// <param name="process"></param>
+        /// <param name="timeout">The amount of time to wait for the associated process to exit.</param>
+        /// <returns>true if the associated process has exited; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static bool WaitForExit(this Process process, TimeSpan timeout) {
+
+            if (process is null)
+                throw new ArgumentNullException(nameof(process));
+
+            return process.WaitForExit((int)timeout.TotalMilliseconds);
+
+        } // .NET 7 and later
+
+    }
+
+}
