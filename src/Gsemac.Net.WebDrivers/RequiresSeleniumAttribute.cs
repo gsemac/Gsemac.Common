97d08da27aeae3bd48b01463aa13af8b4ca79678
gsemac <gabe.semac@gmail.com>
2023-09-15 15:51:52 -0600

    Added `RequiresSeleniumAttribute` class

---
+++ src/Gsemac.Net.WebDrivers/RequiresSeleniumAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Gsemac.Reflection.Plugins;
+using System;
+
+namespace Gsemac.Net.WebDrivers {
+
+    public sealed class RequiresSeleniumAttribute :
+        RequirementAttributeBase {
+
+        // Public members
+
+        public override bool TestRequirement(IServiceProvider serviceProvider) {
+
+            return new RequiresAssemblyOrTypesAttribute("WebDriver", "OpenQA.Selenium.IWebDriver")
+                .TestRequirement(serviceProvider);
+
+        }
+
+    }
+
+}
