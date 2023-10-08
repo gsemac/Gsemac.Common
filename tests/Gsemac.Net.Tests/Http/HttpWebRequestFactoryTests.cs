a20b76caa03473244d3ed4fa8b161b0cb21a6d5a
gsemac <gabe.semac@gmail.com>
2023-10-08 14:46:20 -0600

    Added additional unit tests for `HttpWebRequestFactory`

---
+++ tests/Gsemac.Net.Tests/Http/HttpWebRequestFactoryTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+
+namespace Gsemac.Net.Http.Tests {
+
+    [TestClass]
+    public class HttpWebRequestFactoryTests {
+
+        // Create
+
+        [TestMethod]
+        public void TestRequestsCookieContainerIsNullByDefault() {
+
+            // HttpWebRequest's "CookieContainer" property should be null by default.
+            // If the "cookie" header is set manually, it will only be sent if this property is null.
+
+            IHttpWebRequest webRequest = HttpWebRequestFactory.Default.Create("http://127.0.0.1/");
+
+            Assert.AreEqual(null, webRequest.CookieContainer);
+
+        }
+        [TestMethod]
+        public void TestRequestsProxyIsSetAccordingToHttpWebRequestOptions() {
+
+            // HttpWebRequest's "CookieContainer" property should be null by default.
+            // If the "cookie" header is set manually, it will only be sent if this property is null.
+
+            IWebProxy proxy = new WebProxy("127.0.0.1", 8888);
+
+            IHttpWebRequestFactory webRequestFactory = new HttpWebRequestFactory(new HttpWebRequestOptions() {
+                Proxy = proxy,
+            });
+
+            IHttpWebRequest webRequest = webRequestFactory.Create("http://127.0.0.1/");
+
+            Assert.IsTrue(ReferenceEquals(webRequest.Proxy, proxy));
+
+        }
+
+    }
+
+}
