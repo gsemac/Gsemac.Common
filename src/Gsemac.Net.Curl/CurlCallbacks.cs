4dd8cd3f7fe46dc00c0c509a4baad1687060bc52
gsemac <gabe.semac@gmail.com>
2023-10-31 03:48:16 -0600

    Fixed incorrect calling convention for cURL callback delegates (fixes #12)

--- src/Gsemac.Net.Curl/CurlCallbacks.cs
+++ src/Gsemac.Net.Curl/CurlCallbacks.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Gsemac.Net.Curl {
 
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate UIntPtr ReadFunctionCallback(IntPtr buffer, UIntPtr size, UIntPtr nItems, IntPtr userData);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate UIntPtr WriteFunctionCallback(IntPtr data, UIntPtr size, UIntPtr nMemb, IntPtr userData);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate int ProgressFunctionCallback(IntPtr clientP, double dlTotal, double dlNow, double ulTotal, double ulNow);
 
 }
