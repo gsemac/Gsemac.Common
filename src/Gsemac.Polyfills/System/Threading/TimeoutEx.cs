bf37eee4a41759bde9b0464de66db7772f4b988f
gsemac <gabe.semac@gmail.com>
2024-04-18 17:32:01 -0500

    Added `InfiniteTimeSpan` field

---
+++ src/Gsemac.Polyfills/System/Threading/TimeoutEx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Gsemac.Polyfills.System.Threading {
+
+    public static class TimeoutEx {
+
+        // Public members
+
+        /// <summary>
+        /// A constant used to specify an infinite waiting period, for methods that accept a <see cref="TimeSpan" /> parameter.
+        /// </summary>
+#if NET40_OR_LESSER
+        public static readonly TimeSpan InfiniteTimeSpan = new TimeSpan(0, 0, 0, 0, -1); // Added in .NET Framework 4.5
+#else
+        public static readonly TimeSpan InfiniteTimeSpan = Timeout.InfiniteTimeSpan;
+#endif
+
+    }
+
+}
