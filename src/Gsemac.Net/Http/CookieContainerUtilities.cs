c5b97f44fe700a036462cd44658489001afd159e
gsemac <gabe.semac@gmail.com>
2024-09-10 18:07:07 -0600

    Add `CookieContainerUtilities` class Implemented `RemoveDuplicateCookies` method for removing cookies for duplicated domains from a `CookieContainer` instance.

---
+++ src/Gsemac.Net/Http/CookieContainerUtilities.cs
@@ -0,0 +1,47 @@
+﻿using Gsemac.Polyfills.System.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace Gsemac.Net.Http {
+
+    public static class CookieContainerUtilities {
+
+        // Public members
+
+        public static void RemoveDuplicateCookies(CookieContainer cookieContainer) {
+
+            if (cookieContainer is null)
+                throw new ArgumentNullException(nameof(cookieContainer));
+
+            // CookieContainer has a bug that allows duplicate cookies to exist for "example.com" and ".example.com":
+            // https://stackoverflow.com/q/1047669
+
+            // The two domains are treated as distinct when added to the container, but both will be sent with requests.
+            // This can be avoided by normalizing domains before adding them to the container, but we run into issues for cookies added automatically.
+
+            // There are reflection-based solutions to this, but we will use a different approach to avoid relying on the class' internals too much.
+            // While we can't remove cookies directly, setting them to "Expired" will cause the container to remove them automatically.
+            // We'll keep the latest version of each duplicated cookie, and expire the remaining copies.
+
+            HashSet<Cookie> cookies = new HashSet<Cookie>(new CookieComparer());
+
+            foreach (Cookie cookie in cookieContainer.GetAllCookies().Cast<Cookie>().OrderByDescending(c => c.TimeStamp)) {
+
+                if (cookies.Contains(cookie)) {
+
+                    cookie.Expires = default;
+                    cookie.Expired = true;
+
+                }
+
+                cookies.Add(cookie);
+
+            }
+
+        }
+
+    }
+
+}
---
+++ tests/Gsemac.Net.Tests/Http/CookieContainerUtilitiesTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Net;
+
+namespace Gsemac.Net.Http.Tests {
+
+    [TestClass]
+    public class CookieContainerUtilitiesTests {
+
+        // RemoveDuplicateCookies
+
+        [TestMethod]
+        public void TestRemoveDuplicateCookiesWithDifferentEquivalentDomains() {
+
+            CookieContainer container = new();
+
+            container.Add(new Cookie("name", "value1", "/", "example.com"));
+            container.Add(new Cookie("name", "value2", "/", ".example.com"));
+
+            CookieCollection cookies = container.GetCookies(new Uri("https://example.com/"));
+
+            Assert.AreEqual(2, cookies.Count);
+
+            CookieContainerUtilities.RemoveDuplicateCookies(container);
+
+            cookies = container.GetCookies(new Uri("https://example.com/"));
+
+            Assert.AreEqual(1, cookies.Count);
+            Assert.AreEqual("value2", cookies.First().Value);
+
+        }
+
+    }
+
+}
