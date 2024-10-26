ef3d867b557a03c04b027628f0fa418b9f29d498
gsemac <gabe.semac@gmail.com>
2024-10-26 14:53:02 -0500

    Add `TextBoxUtilities` class

---
+++ src/Gsemac.Forms/TextBoxUtilities.cs
@@ -0,0 +1,32 @@
+﻿using Gsemac.Win32.Native;
+using System;
+using System.Windows.Forms;
+
+namespace Gsemac.Forms {
+
+    public static class TextBoxUtilities {
+
+        // Public members
+
+        public static bool SetPlaceholderText(TextBox textBox, string placeholderText) {
+
+            if (textBox is null)
+                throw new ArgumentNullException(nameof(textBox));
+
+            if (placeholderText is null)
+                placeholderText = string.Empty;
+
+            // Note that the flags required for this feature are available only on Windows Vista or newer.
+
+            if (Environment.OSVersion.Version < new Version(6, 0))
+                return false;
+
+            User32.SendMessage(textBox.Handle, Constants.EM_SETCUEBANNER, (IntPtr)0, placeholderText);
+
+            return true;
+
+        }
+
+    }
+
+}
